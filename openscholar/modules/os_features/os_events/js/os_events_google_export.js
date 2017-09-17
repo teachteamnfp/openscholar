6f1457a35009f183d0b4695d40163d3d13323537
Ethan Mallove <mallove@fas.harvard.edu>
2017-09-17 14:21:53 -0400

    Add "'Overwrite' or 'Create New Calendar'" dialog
     #9441

--- openscholar/modules/os_features/os_events/js/os_events_google_export.js
+++ openscholar/modules/os_features/os_events/js/os_events_google_export.js
@@ -12,9 +12,33 @@
         modal: true,
         buttons: {
           "Overwrite Existing Google Calendar": function() {
+             $.ajax({
+                url     : document.location.pathname + '?overwrite=1',
+                type    : 'POST',
+                data    : '{overwrite:1}',
+                success : function(resp) {
+                  alert("Calendar export complete");
+                  $(this).dialog( "close" );
+                },
+                error   : function(resp){
+                  alert(JSON.stringify(resp));
+                }
+             });
             $( this ).dialog( "close" );
           },
           "Create New Google Calendar": function() {
+             $.ajax({
+                url     : document.location.pathname + '?overwrite=0',
+                type    : 'POST',
+                data    : '{overwrite:0}',
+                success : function(resp) {
+                  alert("Calendar export complete");
+                  $(this).dialog( "close" );
+                },
+                error   : function(resp){
+                  alert(JSON.stringify(resp));
+                }
+             });
             $( this ).dialog( "close" );
           },
           Cancel: function() {
