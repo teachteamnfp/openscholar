5f26b373c9fbaaabf488d1bc04bd0e38c3c0bb3a
Ethan Mallove <mallove@fas.harvard.edu>
2017-09-19 17:42:07 -0400

    Add throbber while AJAX runs, refactor buttons
     * Add throbber while AJAX runs
     * Factor two [nearly identical] buttons into a single type
     * Add `jQuery.once()` call to prevent duplicate runs
    
     #9441

--- openscholar/modules/os_features/os_events/js/os_events_google_export.js
+++ openscholar/modules/os_features/os_events/js/os_events_google_export.js
@@ -5,46 +5,72 @@
    */
   Drupal.behaviors.osPromptForOverwriteOrCreateGoogleCalendar = {
     attach: function () {
-      $( "#export-to-google-calendar-dialog-confirm" ).dialog({
-        resizable: false,
-        height: "auto",
-        width: 600,
-        modal: true,
-        buttons: {
-          "Overwrite Existing Google Calendar": function() {
-             $.ajax({
-                url     : document.location.pathname + '?overwrite=1',
-                type    : 'POST',
-                success : function(resp) {
-                  alert("Calendar export complete");
-                  $(this).dialog( "close" );
-                },
-                error   : function(resp){
-                  alert(JSON.stringify(resp));
-                }
-             });
-            $( this ).dialog( "close" );
-          },
-          "Create New Google Calendar": function() {
-             $.ajax({
-                url     : document.location.pathname + '?overwrite=0',
-                type    : 'POST',
-                success : function(resp) {
-                  alert("Calendar export complete");
-                  $(this).dialog( "close" );
-                },
-                error   : function(resp){
-                  alert(JSON.stringify(resp));
-                }
-             });
-            $( this ).dialog( "close" );
-          },
-          Cancel: function() {
-            $( this ).dialog( "close" );
+
+      function osEventsDialogButton(overwrite, label) {
+
+        var throbber = $("#export-to-google-calendar-throbber").dialog({
+          resizable: false,
+          height: "auto",
+          width: 600,
+          modal: true,
+          show: false,
+          hide: true,
+          autoOpen: false,
+        });
+
+        this.button = {
+          text: label,
+          click: function () {
+            var $this = $(this);
+            $.ajax({
+              url: document.location.pathname + '?overwrite=' + overwrite,
+              type: 'POST',
+              success: function (resp) {
+                alert("Calendar export complete");
+                throbber.dialog("close");
+                $this.dialog("close");
+              },
+              beforeSend: function (xhr, settings) {
+                throbber.dialog("open");
+                $this.dialog("close");
+              },
+              complete: function () {
+                $this.dialog("close");
+                throbber.dialog("close");
+              },
+              error: function (resp) {
+                alert(JSON.stringify(resp));
+              }
+            });
+            $(this).dialog("close");
           }
-        }
+        };
+      }
+
+      var button1 = new osEventsDialogButton(1, "Create New Google Calendar");
+      var button2 = new osEventsDialogButton(0, "Overwrite Existing Google Calendar");
+
+      $("#export-to-google-calendar-throbber").once("export-to-google-calendar-throbber", function () {
+
+        $("#export-to-google-calendar-dialog-confirm").dialog({
+          resizable: false,
+          height: "auto",
+          width: 600,
+          modal: true,
+          buttons: [
+            button1.button,
+            button2.button,
+            {
+              text: "Cancel",
+              click: function () {
+                $(this).dialog("close");
+              }
+            }
+          ]
+        });
       });
+
     }
-  }
+  };
 
 })(jQuery);
