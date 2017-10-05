2b2d3e975ad56d657d903a3dd8896536a46c0306
Matt Petrovic <mpetrovic@iq.harvard.edu>
2017-10-05 15:21:37 -0400

    Correct paths in script

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -40,17 +40,17 @@ cd $BUILD_ROOT
 #List of files from docroot that should be preserved
 preserve_files=( .htaccess robots_disallow.txt sites 404_fast.html favicon.ico )
 #Backup the make files
-cp -f openscholar/openscholar/drupal-org-core.make /tmp/
-cp -f openscholar/openscholar/drupal-org.make /tmp/
-cp -f openscholar/openscholar/bower.json /tmp/
+cp -f openscholar/drupal-org-core.make /tmp/
+cp -f openscholar/drupal-org.make /tmp/
+cp -f openscholar/bower.json /tmp/
 git subtree pull -m "subtree merge in codeship" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH
 #Only build if no build has ever happened, or if the make files have changed
-if [ ! -d openscholar/openscholar/modules/contrib ] || [ $FORCE_REBUILD == "1" ] || [ "$(cmp -b 'openscholar/openscholar/drupal-org-core.make' '/tmp/drupal-org-core.make')" != "" ] || [ "$(cmp -b 'openscholar/openscholar/drupal-org.make' '/tmp/drupal-org.make')" != "" ] || [ "$(cmp -b 'openscholar/openscholar/bower.json' '/tmp/bower.json')" != "" ]; then
+if [ ! -d openscholar/modules/contrib ] || [ $FORCE_REBUILD == "1" ] || [ "$(cmp -b 'openscholar/drupal-org-core.make' '/tmp/drupal-org-core.make')" != "" ] || [ "$(cmp -b 'openscholar/drupal-org.make' '/tmp/drupal-org.make')" != "" ] || [ "$(cmp -b 'openscholar/bower.json' '/tmp/bower.json')" != "" ]; then
 # Chores.
-for DIR in $BUILD_ROOT/www-build $BUILD_ROOT/www-backup openscholar/openscholar/1 openscholar/openscholar/modules/contrib openscholar/openscholar/themes/contrib openscholar/openscholar/libraries; do
+for DIR in $BUILD_ROOT/www-build $BUILD_ROOT/www-backup openscholar/1 openscholar/modules/contrib openscholar/themes/contrib openscholar/libraries; do
 	rm -Rf $DIR
 done
-cd openscholar/openscholar
+cd openscholar
 
 $DRUSH make --no-core --contrib-destination drupal-org.make .
 (
@@ -68,7 +68,7 @@ $DRUSH make --no-core --contrib-destination drupal-org.make .
 )
 
 cd ../../
-$DRUSH make openscholar/openscholar/drupal-org-core.make $BUILD_ROOT/www-build
+$DRUSH make openscholar/drupal-org-core.make $BUILD_ROOT/www-build
 
 # Backup files from existing installation.
 cd $BUILD_ROOT
@@ -78,11 +78,11 @@ for BACKUP_FILE in "${preserve_files[@]}"; do
 	mv $DOCROOT/$BACKUP_FILE www-build/
 done
 # Move the profile in place.
-ln -s ../../openscholar/openscholar $BUILD_ROOT/www-build/profiles/openscholar
+ln -s ../../openscholar $BUILD_ROOT/www-build/profiles/openscholar
 # link up phpmyadmin
 # ln -s ../phpMyAdmin-3.5.2.2-english $BUILD_ROOT/www-build/phpmyadmin
 #link up js.php
-ln -s ../openscholar/openscholar/modules/contrib/js/js.php $BUILD_ROOT/www-build/js.php
+ln -s ../openscholar/modules/contrib/js/js.php $BUILD_ROOT/www-build/js.php
 # Fix permissions before deleting.
 # chmod -R +w $BUILD_ROOT/$DOCROOT/sites/* || true
 rm -Rf $BUILD_ROOT/$DOCROOT || true
@@ -96,10 +96,10 @@ git add --all $BUILD_ROOT/$DOCROOT
 cp -R openscholar/temporary/* openscholar/openscholar/modules/contrib/
 # iCalcreator cannot be downloaded via make because a temporary token is needed,
 # so we have the library inside os_events directory and we copy it to libraries.
-cp -R openscholar/openscholar/modules/os_features/os_events/iCalcreator openscholar/openscholar/libraries/
+cp -R openscholar/modules/os_features/os_events/iCalcreator openscholar/libraries/
 # Download the git wrapper library using the composer.
 
-for DIR in openscholar/openscholar/libraries openscholar/openscholar/themes/contrib openscholar/openscholar/modules/contrib
+for DIR in openscholar/libraries openscholar/themes/contrib openscholar/modules/contrib
 do
 if [ -d "$DIR" ]; then
 git add --all -f $DIR
@@ -109,9 +109,8 @@ git commit -a -m "Make File Update."
 #END BUILD PROCESS
 else
 #Copy unmakable modules, when we don’t build
-cp -R openscholar/temporary/* openscholar/openscholar/modules/contrib/
+cp -R temporary/* openscholar/modules/contrib/
 git commit -a -m "Update Temporary Modules." || echo 'Nothing to commit.'
 fi
 git push origin $CI_BRANCH
 echo -e "\033[1;36mFINISHED BUILDING $CI_BRANCH ON BITBUCKET\e[0m"
-
