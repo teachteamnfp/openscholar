804874908d3eba726d78f4e13f5a0e550891d9ad
“dancabral” <rcabral@fas.harvard.edu>
2018-03-02 12:18:46 -0500

    #10186 switch bc theme

--- openscholar/themes/hwpi_themethree_bostoncollege/script.js
+++ openscholar/themes/hwpi_themethree_bostoncollege/script.js
@@ -1,6 +1,6 @@
-jQuery('div.branding-left a').replaceWith('<a href="https://www.bentley.edu/"><img src="/profiles/openscholar/themes/hwpi_themethree_bentley/images/bentley-logo.png" alt="Harvard Logo"></a>');
-jQuery('div.copyright').replaceWith('Copyright © 2018 Bentley University');
-jQuery('div.branding-right a:last-of-type').replaceWith('<a href="https://www.bentley.edu/">bentley.edu</a>');
-jQuery('div.field-name-field-person-photo').replaceWith('<div class="field-name-field-person-photo"><img src="/profiles/openscholar/themes/hwpi_themethree_bentley/images/avatar.png" alt=""></div>');
+jQuery('div.branding-left a').replaceWith('<a href="https://www.american.edu/"><img src="/profiles/openscholar/themes/hwpi_themethree_american/images/logo.png" alt="American Logo"></a>');
+jQuery('div.copyright').replaceWith('Copyright © 2018 American University');
+jQuery('div.branding-right a:last-of-type').replaceWith('<a href="https://www.american.edu/">american.edu</a>');
+jQuery('div.field-name-field-person-photo').replaceWith('<div class="field-name-field-person-photo"><img src="/profiles/openscholar/themes/hwpi_themethree_american/images/avatar.png" alt=""></div>');
 
 
