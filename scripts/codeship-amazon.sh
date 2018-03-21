6a95be011b50ceb8a8302f4020b61f7fc9cb3c29
mpetrovic <mpetrovic@guneva.net>
2018-03-21 13:20:52 -0400

    #10228: Try to commit custom code

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -12,7 +12,7 @@ function buildComposer() {
         echo "Installing site-specific modules for $site"
         composer -vvv install
         ls -l $1/$2/sites/$site
-        git add -A $1/$2/sites/$site/modules/
+        git add -A $1/$2/sites/$site/modules/*
         cd -
     done
     cd $ORIG
