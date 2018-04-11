c784b98fedaf49faf425ae67e2072782d517b360
mpetrovic <mpetrovic@guneva.net>
2018-04-11 11:40:47 -0400

    #10228: Add special <base_domain> token for redirecting to root of install. Use it when deleting the current site.

--- openscholar/modules/frontend/admin_panel/misc/UserPanelModule.js
+++ openscholar/modules/frontend/admin_panel/misc/UserPanelModule.js
@@ -54,6 +54,8 @@
     });
     $scope.pageSize = 7;
     if (Drupal.settings.spaces) {
+      $scope.spaceId = Drupal.settings.spaces.id;
+      $scope.delete_destination_root = '?destination=<base_domain>';
       $scope.delete_destination = '?'+encodeURIComponent('destination=node/' + Drupal.settings.spaces.id + encodeURIComponent('?destination=' + window.location.pathname.replace(/^\/|\/$/g, '')));
     } else {
       $scope.delete_destination = '';
