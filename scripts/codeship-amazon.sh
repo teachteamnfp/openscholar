3d2ee2936048f39437f64beabbbb4649b9e0ffce
mpetrovic <mpetrovic@guneva.net>
2018-05-15 14:01:22 -0400

    #10335: Catch hyphens

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -15,7 +15,7 @@ function buildComposer() {
         cd openscholar/sites/$site
         echo "Installing site-specific modules for $site"
         composer install -n
-        MODULE=$(composer show -s | sed -e '1,/requires/d' | sed -e 's| [^\s]*$||')
+        MODULE=$(composer show -s | sed -e '1,/requires/d' | sed -e 's| [^\s-]*$||')
         echo "Checking out $MODULE"
         cd $1/$2/sites/$site/modules/$MODULE
         git branch | grep -v "master" | xargs git branch -D
