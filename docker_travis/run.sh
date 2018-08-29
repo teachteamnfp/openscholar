c48c31e2fecaf932ae63e01df30b22543da5b854
Sztreha Balázs <balazs.sztreha@cheppers.com>
2018-08-29 13:55:17 +0200

    Issue #10414 - TRY remove debug info

--- docker_travis/run.sh
+++ docker_travis/run.sh
@@ -2,8 +2,7 @@
 
 echo -e "\n # Start services and run behat tests ..."
 systemctl start httpd
-systemctl status httpd
-journalctl -u httpd
+
 cd /opt/apache-solr/apache-solr-3.6.2/example/solr/conf
 yes | cp /var/www/html/www/profiles/openscholar/modules/contrib/apachesolr/solr-conf/solr-3.x/* .
 yes | cp /var/www/html/www/profiles/openscholar/behat/solr/solrconfig.xml .
