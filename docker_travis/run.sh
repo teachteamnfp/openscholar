f02caef57d593241c413b6a1f52dcbc1572b4ba0
Sztreha Balázs <balazs.sztreha@cheppers.com>
2018-08-31 16:51:02 +0200

    Issue #10414 - FIX vsite tag test, enable run testing

--- docker_travis/run.sh
+++ docker_travis/run.sh
@@ -8,6 +8,12 @@ yes | cp /var/www/html/www/profiles/openscholar/modules/contrib/apachesolr/solr-
 yes | cp /var/www/html/www/profiles/openscholar/behat/solr/solrconfig.xml .
 cd /opt/apache-solr/apache-solr-3.6.2/example
 java -jar start.jar &
+
+cd /var/www/html/openscholar/behat
+sh -c "echo 127.0.0.1  lincoln.local >> /etc/hosts"
+sh -c "cat lincoln-vhost.txt > /etc/httpd/conf.d/lincoln.local.conf"
+systemctl restart httpd
+
 Xvfb :99 -ac &
 export DISPLAY=:99
 sleep 5
@@ -34,7 +40,7 @@ cp behat.local.yml.travis behat.local.yml
 
 # Run tests
 echo -e "\n # Run tests"
-#./bin/behat --tags="${TEST_SUITE}" --strict
+./bin/behat --tags="${TEST_SUITE}" --strict
 
 if [ $? -ne 0 ]; then
   echo "Behat failed"
