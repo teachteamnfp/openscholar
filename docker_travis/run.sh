500926da7ec12e5c5f921ed6bbbe6ff4bfede65e
Sztreha Balázs <balazs.sztreha@cheppers.com>
2018-09-04 15:37:03 +0200

    Issue #10414 - TRY disable apache solr start, remove some tests temporary

--- docker_travis/run.sh
+++ docker_travis/run.sh
@@ -7,8 +7,8 @@ cd /opt/apache-solr/apache-solr-3.6.2/example/solr/conf
 yes | cp /var/www/html/www/profiles/openscholar/modules/contrib/apachesolr/solr-conf/solr-3.x/* .
 yes | cp /var/www/html/www/profiles/openscholar/behat/solr/solrconfig.xml .
 cd /opt/apache-solr/apache-solr-3.6.2/example
-java -jar start.jar &
-sleep 10
+#java -jar start.jar &
+#sleep 10
 
 cd /var/www/html/openscholar/behat
 sh -c "echo 127.0.0.1  lincoln.local >> /etc/hosts"
