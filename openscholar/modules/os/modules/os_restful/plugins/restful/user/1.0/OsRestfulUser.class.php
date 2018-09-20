cd9ec153a63266a46eac0c91f8b62c095320803a
mpetrovic <mpetrovic@guneva.net>
2018-09-20 10:16:37 -0400

    #10758: Remove extra parenthesis

--- openscholar/modules/os/modules/os_restful/plugins/restful/user/1.0/OsRestfulUser.class.php
+++ openscholar/modules/os/modules/os_restful/plugins/restful/user/1.0/OsRestfulUser.class.php
@@ -129,7 +129,7 @@ class OsRestfulUser extends \RestfulEntityBaseUser {
       drupal_save_session(true);
       user_login_finalize();
       // TODO: Remove this with saml
-      if (module_exists('pinserver_authenticate') && $huid = pinserver_get_user_huid () && !pinserver_authenticate_get_uid_from_huid () ()) {
+      if (module_exists('pinserver_authenticate') && $huid = pinserver_get_user_huid () && !pinserver_authenticate_get_uid_from_huid ()) {
         pinserver_authenticate_set_user_huid ($user->uid, $huid);
       }
       return $this->viewEntity($user->uid);
