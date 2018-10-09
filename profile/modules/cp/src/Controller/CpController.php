08fc9b6ef4af20b100caaf681da3b25918445018
mpetrovic <mpetrovic@guneva.net>
2018-10-09 16:14:16 -0400

    Control Panel Toolbar proof of concept

---
+++ profile/modules/cp/src/Controller/CpController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\cp\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class CpController extends ControllerBase  {
+
+  public function cpAdminMenuBlockPage() {
+    return [
+      '#markup' => 'Nothing here yet',
+    ];
+  }
+
+  public function dummyPage() {
+    return [
+      '#markup' => 'Just a placeholder page'
+    ];
+  }
+}
