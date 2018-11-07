1d56498202b2fe203826ca51fbb865b420b1e273
mpetrovic <mpetrovic@guneva.net>
2018-11-07 15:08:45 -0500

    #10910: New method on app manager to get the app for a bundle

--- profile/modules/vsite/src/Plugin/AppManangerInterface.php
+++ profile/modules/vsite/src/Plugin/AppManangerInterface.php
@@ -6,4 +6,6 @@ namespace Drupal\vsite\Plugin;
 use Drupal\Component\Plugin\PluginManagerInterface;
 
 interface AppManangerInterface extends PluginManagerInterface {
+
+  public function getAppForBundle(string $bundle) : string;
 }
