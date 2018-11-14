d6fcd54ac5266f93fb9da6850b7b7445035dfc59
Sztreha Balázs <balazs.sztreha@cheppers.com>
2018-11-14 16:07:36 +0100

    Issue #10896 - Minimal optimization

--- profile/modules/vsite/src/Access/VsiteGroupPermissionAccessCheck.php
+++ profile/modules/vsite/src/Access/VsiteGroupPermissionAccessCheck.php
@@ -23,15 +23,14 @@ class VsiteGroupPermissionAccessCheck extends GroupPermissionAccessCheck {
    *   The access result.
    */
   public function access(Route $route, RouteMatchInterface $route_match, AccountInterface $account) {
-    $current_route_match = \Drupal::service('current_route_match');
-    $currentParameter = $current_route_match->getParameters();
-    $parameterBag = $route_match->getParameters();
     /** @var \Drupal\group\Entity\Group $group */
     if ($group = \Drupal::service('vsite.context_manager')->getActiveVsite()) {
+      $current_route_match = \Drupal::service('current_route_match');
+      $currentParameter = $current_route_match->getParameters();
+      $parameterBag = $route_match->getParameters();
       $parameterBag->add(['group' => $group]);
       $currentParameter->add(['group' => $group]);
     }
     return parent::access($route, $route_match, $account);
   }
-
 }
