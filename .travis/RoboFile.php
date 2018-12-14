513636ee9d397b8adb4d8932e64d7d64dc40c470
Subhojit Paul <subhojitpaul21@gmail.com>
2018-12-14 17:43:16 +0530

    Issue #10943: Trial and Error - Fix Travis tests breaking

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -275,7 +275,8 @@ class RoboFile extends \Robo\Tasks
         $tasks = [];
         $tasks[] = $this->taskFilesystemStack()
             ->copy('.travis/config/phpunit.xml', 'web/core/phpunit.xml', TRUE)
-            ->copy('.travis/config/bootstrap.php', 'web/core/tests/bootstrap.php', TRUE);
+            ->copy('.travis/config/bootstrap.php', 'web/core/tests/bootstrap.php', TRUE)
+            ->mkdir('web/sites/simpletest');
         $tasks[] = $this->taskExecStack()
             ->dir('web')
             ->exec('../vendor/bin/phpunit '.
