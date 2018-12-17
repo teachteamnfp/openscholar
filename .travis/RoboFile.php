2531df5cbfa001a8bcb24aa73c4dcbf21818baae
Subhojit Paul <subhojitpaul21@gmail.com>
2018-12-17 12:13:53 +0530

    Revert "Issue #10943: Issue #10943: Trial and error - Fix Travis cannot find db for site install"
    This reverts commit f9942f99785bf12237a8ff0352b4042ea5af7b81.

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -17,7 +17,7 @@ class RoboFile extends \Robo\Tasks
     /**
      * The database URL.
      */
-    const DB_URL = 'mysql://drupal:drupal@mariadb/drupal';
+    const DB_URL = 'sqlite://tmp/site.sqlite';
 
     /**
      * The website's URL.
