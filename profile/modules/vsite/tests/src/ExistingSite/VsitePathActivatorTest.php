f67dbd15f6e3729c7df31da815ef7075c5807898
Subhojit Paul <subhojitpaul21@gmail.com>
2018-12-20 17:51:46 +0530

    Issue #10943: Added test for VsitePathActivator::onModifierMatched

--- profile/modules/vsite/tests/src/ExistingSite/VsitePathActivatorTest.php
+++ profile/modules/vsite/tests/src/ExistingSite/VsitePathActivatorTest.php
@@ -21,11 +21,19 @@ class VsitePathActivatorTest extends ExistingSiteBase {
   protected $entityTypeManager;
 
   /**
+   * The storage element to add a vsite storage to.
+   *
+   * @var \Drupal\vsite\Config\HierarchicalStorageInterface
+   */
+  protected $hierarchicalStorage;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
     $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->hierarchicalStorage = $this->container->get('hierarchical.storage');
   }
 
   /**
@@ -51,16 +59,21 @@ class VsitePathActivatorTest extends ExistingSiteBase {
    * Tests modifier matched event.
    */
   public function testModifierMatched() {
-    $this->createGroup([
+    $group = $this->createGroup([
       'type' => 'personal',
       'path' => [
         'alias' => '/test-alias',
       ],
     ]);
 
-    // TODO: This should hit /test-alias.
-    $this->drupalGet('<front>');
+    $this->drupalGet('/test-alias');
+    // TODO: Fix this failing test.
+    // TODO: This should be returning 500.
+    // TODO: Found that inside Symfony's RouteListener that it is returning 403,
+    // TODO: but due to unknown reason Drupal is coverting it to 500.
+    // TODO: Find and fix the problem.
     $this->assertSession()->statusCodeEquals(200);
+    $this->assertEquals("vsite:{$group->id()}", $this->hierarchicalStorage->getCollectionName());
   }
 
 }
