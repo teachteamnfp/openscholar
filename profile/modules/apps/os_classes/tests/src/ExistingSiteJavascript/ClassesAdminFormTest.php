5e717cf5380d51f96c33d0778003e4312a50b088
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-01-15 17:46:20 +0100

    Issue #10909 - Test default value of admin form

--- profile/modules/apps/os_classes/tests/src/ExistingSiteJavascript/ClassesAdminFormTest.php
+++ profile/modules/apps/os_classes/tests/src/ExistingSiteJavascript/ClassesAdminFormTest.php
@@ -36,19 +36,17 @@ class ClassesAdminFormTest extends ClassesExistingSiteJavascriptTestBase {
    * Tests os_classes admin form access.
    */
   public function testAccessAdminForm() {
-
     $user = $this->createUser(['administer site configuration']);
     $this->drupalLogin($user);
     $this->visit('/admin/config/openscholar/classes/field-allowed-values');
 
     $web_assert = $this->assertSession();
 
-    try {
-      $web_assert->statusCodeEquals(200);
-    }
-    catch (ExpectationException $e) {
-      $this->fail(sprintf("Test failed: %s\nBacktrace: %s", $e->getMessage(), $e->getTraceAsString()));
-    }
+    $web_assert->statusCodeEquals(200);
+    $web_assert->pageTextContains('Semester field options');
+    $page = $this->getCurrentPage();
+    $checkDefaultValue = $page->hasContent('fall|Fall');
+    $this->assertTrue($checkDefaultValue, 'Default value is not loaded on admin form.');
   }
 
 }
