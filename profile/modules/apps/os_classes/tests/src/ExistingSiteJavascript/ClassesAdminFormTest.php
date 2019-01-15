6e46e3f7b888352c053932c09177c25813eb2379
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-01-15 15:40:17 +0100

    Issue #10909 - Create access check tests for admin form

--- profile/modules/apps/os_classes/tests/src/ExistingSiteJavascript/ClassesAdminFormTest.php
+++ profile/modules/apps/os_classes/tests/src/ExistingSiteJavascript/ClassesAdminFormTest.php
@@ -16,11 +16,32 @@ class ClassesAdminFormTest extends ClassesExistingSiteJavascriptTestBase {
   /**
    * Tests os_classes admin form access.
    */
-  public function testAccessAdminForm() {
+  public function testAccessDeniedAdminForm() {
+
+    // Create a non-admin user
+    $user = $this->createUser();
+    $this->drupalLogin($user);
+    $this->visit('/admin/config/openscholar/classes/field-allowed-values');
 
     $web_assert = $this->assertSession();
 
+    try {
+      $web_assert->statusCodeEquals(403);
+    }
+    catch (ExpectationException $e) {
+      $this->fail(sprintf("Test failed: %s\nBacktrace: %s", $e->getMessage(), $e->getTraceAsString()));
+    }
+  }
+  /**
+   * Tests os_classes admin form access.
+   */
+  public function testAccessAdminForm() {
+
+    $user = $this->createUser(['administer site configuration']);
+    $this->drupalLogin($user);
     $this->visit('/admin/config/openscholar/classes/field-allowed-values');
+
+    $web_assert = $this->assertSession();
 
     try {
       $web_assert->statusCodeEquals(200);
