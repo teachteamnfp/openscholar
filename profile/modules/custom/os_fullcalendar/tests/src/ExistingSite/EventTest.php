c973d034a665c408b446408bf38c1bcad23caa42
Subhojit Paul <subhojitpaul21@gmail.com>
2019-01-16 12:27:47 +0530

    Issue #10906: Event URL alias test

---
+++ profile/modules/custom/os_fullcalendar/tests/src/ExistingSite/EventTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\os_fullcalendar\ExistingSite;
+
+/**
+ * Event tests.
+ *
+ * @group vsite
+ * @group functional
+ */
+class EventTest extends EventTestBase {
+
+  /**
+   * Tests event alias.
+   *
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
+   */
+  public function testEventAlias() {
+    /** @var \Drupal\Core\Path\AliasManagerInterface $alias_manager */
+    $alias_manager = $this->container->get('path.alias_manager');
+    /** @var \Drupal\vsite\Plugin\VsiteContextManagerInterface $vsite_context_manager */
+    $vsite_context_manager = $this->container->get('vsite.context_manager');
+
+    $vsite_context_manager->activateVsite($this->group);
+
+    /** @var \Drupal\node\NodeInterface $event */
+    $event = $this->createEvent([
+      'title' => 'Test Unique Event',
+      'field_groups' => [
+        'target_id' => $this->group->id(),
+      ],
+      'status' => TRUE,
+    ]);
+    $this->group->addContent($event, "group_node:{$event->bundle()}");
+
+    $this->assertEquals($alias_manager->getAliasByPath("/node/{$event->id()}"), "{$this->group->get('path')->first()->getValue()['alias']}/event/test-unique-event");
+  }
+
+}
---
+++ profile/modules/custom/os_fullcalendar/tests/src/ExistingSite/EventTestBase.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\Tests\os_fullcalendar\ExistingSite;
+
+use Drupal\group\Entity\GroupInterface;
+use Drupal\node\NodeInterface;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Test base for event tests.
+ */
+class EventTestBase extends ExistingSiteBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Test group.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
+   */
+  protected $group;
+
+  /**
+   * Test event.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $event;
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->config = $this->container->get('config.factory');
+
+    $this->group = $this->createGroup([
+      'path' => [
+        'alias' => '/test-alias',
+      ],
+    ]);
+    $this->event = $this->createEvent([
+      'title' => 'Test Event',
+      'field_groups' => [
+        'target_id' => $this->group->id(),
+      ],
+      'field_recurring_date' => [
+        'value' => date("Y-m-d\TH:i:s", strtotime("today midnight")),
+        'end_value' => date("Y-m-d\TH:i:s", strtotime("tomorrow midnight")),
+        'rrule' => '',
+        'timezone' => $this->config->get('system.date')->get('timezone.default'),
+        'infinite' => FALSE,
+      ],
+      'status' => TRUE,
+    ]);
+  }
+
+  /**
+   * Creates a group.
+   *
+   * @param array $values
+   *   (optional) The values used to create the entity.
+   *
+   * @return \Drupal\group\Entity\GroupInterface
+   *   The created group entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   */
+  protected function createGroup(array $values = []) : GroupInterface {
+    $group = $this->entityTypeManager->getStorage('group')->create($values + [
+      'type' => 'personal',
+      'label' => $this->randomMachineName(),
+    ]);
+    $group->enforceIsNew();
+    $group->save();
+
+    $this->markEntityForCleanup($group);
+
+    return $group;
+  }
+
+  /**
+   * Creates an event.
+   *
+   * @param array $values
+   *   The values used to create the event.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created event entity.
+   */
+  protected function createEvent(array $values = []) : NodeInterface {
+    $event = $this->createNode($values + [
+      'type' => 'events',
+      'title' => $this->randomString(),
+    ]);
+
+    return $event;
+  }
+
+}
