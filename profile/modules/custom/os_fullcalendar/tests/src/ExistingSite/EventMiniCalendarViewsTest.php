2ee1a38988bd6fb571c509e1f8d3137f6ef89fa5
Subhojit Paul <subhojitpaul21@gmail.com>
2019-01-17 15:02:58 +0530

    Issue #10906: Converted mini calendar tests from functional to kernel

--- profile/modules/custom/os_fullcalendar/tests/src/ExistingSite/EventMiniCalendarViewsTest.php
+++ profile/modules/custom/os_fullcalendar/tests/src/ExistingSite/EventMiniCalendarViewsTest.php
@@ -3,19 +3,48 @@
 namespace Drupal\Tests\os_fullcalendar\ExistingSite;
 
 /**
- * Tests upcoming events calendar view.
+ * Tests mini calendar views.
  *
  * @group vsite
  * @group kernel
  */
-class EventCalendarUpcomingViewTest extends EventTestBase {
+class EventMiniCalendarViewsTest extends EventTestBase {
 
   /**
-   * Tests the block.
+   * Tests monthly calendar view.
    */
-  public function testBlock() {
-    /** @var \Drupal\node\NodeInterface $past_event */
-    $past_event = $this->createEvent([
+  public function testMonthlyCalendarView() {
+    $this->createEvent([
+      'title' => 'Next Month Event',
+      'field_groups' => [
+        'target_id' => $this->group->id(),
+      ],
+      'field_recurring_date' => [
+        'value' => date("Y-m-d\TH:i:s", strtotime("+1 month midnight")),
+        'end_value' => date("Y-m-d\TH:i:s", strtotime("+1 day +1 month midnight")),
+        'rrule' => '',
+        'timezone' => $this->config->get('system.date')->get('timezone.default'),
+        'infinite' => FALSE,
+      ],
+      'status' => TRUE,
+    ]);
+
+    /** @var array $result */
+    $result = views_get_view_result('calendar', 'block_1', $this->group->id());
+
+    // Next month event should not appear.
+    $this->assertCount(1, $result);
+    /** @var \Drupal\views\ResultRow $item */
+    foreach ($result as $item) {
+      $this->assertEquals($this->event->id(), $item->nid);
+    }
+  }
+
+  /**
+   * Tests upcoming events calendar view.
+   */
+  public function testUpcomingEventsCalendarView() {
+    $this->createEvent([
       'title' => 'Past Event',
       'field_groups' => [
         'target_id' => $this->group->id(),
