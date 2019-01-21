9121272776ce82c634f6d9d592bee8e376f81383
Subhojit Paul <subhojitpaul21@gmail.com>
2019-01-21 17:06:30 +0530

    Issue #10908: Test book outline

--- profile/tests/src/ExistingSite/PagesTest.php
+++ profile/tests/src/ExistingSite/PagesTest.php
@@ -25,4 +25,44 @@ class PagesTest extends TestBase {
     $this->assertEquals($alias_manager->getAliasByPath("/node/{$book->id()}"), '/first-book');
   }
 
+  /**
+   * Tests book outline.
+   */
+  public function testOutline() {
+    /** @var \Drupal\node\NodeInterface $book */
+    $book = $this->createBookPage();
+    $this->bookManager->updateOutline($book);
+
+    /** @var \Drupal\node\NodeInterface $page1 */
+    $page1 = $this->createBookPage([], $book->id());
+    $this->bookManager->updateOutline($page1);
+
+    /** @var \Drupal\node\NodeInterface $page11 */
+    $page11 = $this->createBookPage([], $book->id(), $page1->id());
+    $this->bookManager->updateOutline($page11);
+
+    /** @var \Drupal\node\NodeInterface $page2 */
+    $page2 = $this->createBookPage([], $book->id());
+    $this->bookManager->updateOutline($page2);
+
+    // Assert book has no parent and has correct number of children.
+    $this->assertEquals(0, $book->book['pid']);
+    $this->assertCount(2, $this->bookOutlineStorage->loadBookChildren($book->id()));
+
+    // Assert page1 is placed correctly in the hierarchy.
+    $this->assertEquals($book->id(), $page1->book['pid']);
+    $this->assertEquals($book->id(), $page1->book['bid']);
+    $this->assertCount(1, $this->bookOutlineStorage->loadBookChildren($page1->id()));
+
+    // Assert page11 is placed correctly in the hierarchy.
+    $this->assertEquals($page1->id(), $page11->book['pid']);
+    $this->assertEquals($book->id(), $page11->book['bid']);
+    $this->assertCount(0, $this->bookOutlineStorage->loadBookChildren($page11->id()));
+
+    // Assert page2 is placed correctly in the hierarchy.
+    $this->assertEquals($book->id(), $page2->book['pid']);
+    $this->assertEquals($book->id(), $page2->book['bid']);
+    $this->assertCount(0, $this->bookOutlineStorage->loadBookChildren($page2->id()));
+  }
+
 }
--- profile/tests/src/ExistingSite/TestBase.php
+++ profile/tests/src/ExistingSite/TestBase.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\openscholar\ExistingSite;
 
 use Drupal\node\NodeInterface;
+use Drupal\Tests\book\Functional\BookTestTrait;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
@@ -10,24 +11,56 @@ use weitzman\DrupalTestTraits\ExistingSiteBase;
  */
 abstract class TestBase extends ExistingSiteBase {
 
+  use BookTestTrait;
+
+  /**
+   * The book outline storage.
+   *
+   * @var \Drupal\book\BookOutlineStorageInterface
+   */
+  protected $bookOutlineStorage;
+
+  /**
+   * The book manager.
+   *
+   * @var \Drupal\book\BookManagerInterface
+   */
+  protected $bookManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->bookOutlineStorage = $this->container->get('book.outline_storage');
+    $this->bookManager = $this->container->get('book.manager');
+  }
+
   /**
    * Creates a book page.
    *
    * @param array $values
-   *   Initial values.
+   *   (Optional) Initial values. If nothing is passed, the page is created with
+   *   default values.
    * @param int|null $book_id
    *   (Optional) Book id where this page should be attached. If nothing is
    *   passed, this creates a new book.
+   * @param int|null $parent_id
+   *   (Optional) The parent id. If nothing is passed, the page will become the
+   *   first page.
    *
    * @return \Drupal\node\NodeInterface
    *   The new entity.
    */
-  public function createBookPage(array $values, $book_id = NULL) : NodeInterface {
+  public function createBookPage(array $values = [], $book_id = NULL, $parent_id = NULL) : NodeInterface {
     /** @var \Drupal\node\NodeInterface $book */
     $book = $this->createNode($values + [
       'type' => 'page',
       'book' => [
-        'bid' => $book_id ?: 'new',
+        'bid' => $book_id ?? 'new',
+        'pid' => $parent_id ?? -1,
+        'weight' => 0,
       ],
     ]);
 
