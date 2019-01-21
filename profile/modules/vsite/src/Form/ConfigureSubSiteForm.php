b9636fe3c848caacfbab49804f97d30d27aa34d8
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-01-21 14:02:55 +0100

    Issue #10904 - DI implementation

--- profile/modules/vsite/src/Form/ConfigureSubSiteForm.php
+++ profile/modules/vsite/src/Form/ConfigureSubSiteForm.php
@@ -2,13 +2,50 @@
 
 namespace Drupal\vsite\Form;
 
+use Drupal\Core\Entity\EntityTypeBundleInfo;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\Messenger;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Implements a configuration form.
  */
 class ConfigureSubSiteForm extends FormBase {
+
+  /**
+   * Entity type bundle info.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfo
+   */
+  private $entityTypeBundleInfo;
+
+  /**
+   * Drupal core messenger.
+   *
+   * @var \Drupal\Core\Messenger\Messenger
+   */
+  private $coreMessenger;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(EntityTypeBundleInfo $entity_type_bundle_info, Messenger $coreMessenger) {
+    $this->entityTypeBundleInfo = $entity_type_bundle_info;
+    $this->coreMessenger = $coreMessenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+    // Load the service required to construct this class.
+      $container->get('entity_type.bundle.info'),
+      $container->get('messenger')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -21,16 +58,16 @@ class ConfigureSubSiteForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $group_bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo('group');
+    $group_bundles = $this->entityTypeBundleInfo->getBundleInfo('group');
 
     $allowed_parent_site_options = [];
     $defaultAllowedValues = [];
     foreach ($group_bundles as $bundle_name => $bundle) {
       $allowed_parent_site_options[$bundle_name] = $bundle['label'];
 
-      $field_config = \Drupal::config('field.field.group.' . $bundle_name . '.field_parent_site')->get('settings');
+      $field_config = $this->config('field.field.group.' . $bundle_name . '.field_parent_site')->get('settings');
       if (empty($field_config)) {
-        \Drupal::messenger()->addWarning(t('Group type %type is missing field_parent_site!', ['%type' => $bundle['label']]));
+        $this->coreMessenger->addWarning(t('Group type %type is missing field_parent_site!', ['%type' => $bundle['label']]));
       }
       if (!empty($field_config['handler_settings']['target_bundles'])) {
         foreach ($field_config['handler_settings']['target_bundles'] as $target_bundle) {
@@ -47,7 +84,7 @@ class ConfigureSubSiteForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    $vsite_config = \Drupal::config('vsite.settings')->get('allowed_subsite_group_types');
+    $vsite_config = $this->config('vsite.settings')->get('allowed_subsite_group_types');
 
     $form['allowed_sub_sites'] = [
       '#title' => $this->t('Allowed sub sites group bundles'),
@@ -88,27 +125,27 @@ class ConfigureSubSiteForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $allowed_parent_values = $this->normalizeValues($form_state->getValue('allowed_parent_sites'));
 
-    $group_bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo('group');
+    $group_bundles = $this->entityTypeBundleInfo->getBundleInfo('group');
 
     // Update field parent site field instance config
     // in all group entity bundle.
     foreach ($group_bundles as $bundle_name => $bundle) {
-      $field_config = \Drupal::config('field.field.group.' . $bundle_name . '.field_parent_site')->get('settings');
+      $field_config = $this->config('field.field.group.' . $bundle_name . '.field_parent_site')->get('settings');
       if (empty($field_config)) {
         continue;
       }
       $field_config['handler_settings']['target_bundles'] = $allowed_parent_values;
-      \Drupal::configFactory()->getEditable('field.field.group.' . $bundle_name . '.field_parent_site')
+      $this->configFactory->getEditable('field.field.group.' . $bundle_name . '.field_parent_site')
         ->set('settings', $field_config)
         ->save();
     }
 
     $allowed_subsite_values = $this->normalizeValues($form_state->getValue('allowed_sub_sites'));
-    \Drupal::configFactory()->getEditable('vsite.settings')
+    $this->configFactory->getEditable('vsite.settings')
       ->set('allowed_subsite_group_types', $allowed_subsite_values)
       ->save();
 
-    \Drupal::messenger()->addMessage(t('Allowed values settings saved successful.'));
+    $this->coreMessenger->addMessage(t('Allowed values settings saved successful.'));
   }
 
   /**
