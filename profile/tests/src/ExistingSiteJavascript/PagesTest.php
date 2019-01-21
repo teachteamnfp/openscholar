c9d5eee9f84fec2fb95f3b7a2fb26587d46272f1
Subhojit Paul <subhojitpaul21@gmail.com>
2019-01-21 17:51:06 +0530

    Issue #10908: Book outline visibility test

---
+++ profile/tests/src/ExistingSiteJavascript/PagesTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\Tests\openscholar\ExistingSiteJavascript;
+
+use Behat\Mink\Exception\ExpectationException;
+
+/**
+ * Tests book pages.
+ *
+ * @group openscholar
+ * @group functional-javascript
+ */
+class PagesTest extends TestBase {
+
+  /**
+   * Tests visibility of book outline.
+   */
+  public function testBookVisibility() {
+    /** @var \Drupal\book\BookManagerInterface $book_manager */
+    $book_manager = $this->container->get('book.manager');
+    /** @var \Drupal\Core\Path\AliasManagerInterface $path_alias_manager */
+    $path_alias_manager = $this->container->get('path.alias_manager');
+
+    /** @var \Drupal\node\NodeInterface $book1 */
+    $book1 = $this->createBookPage([
+      'title' => 'Harry Potter and the Philosophers Stone',
+    ]);
+    $book_manager->updateOutline($book1);
+
+    /** @var \Drupal\node\NodeInterface $page1 */
+    $page1 = $this->createBookPage([
+      'title' => 'The Boy Who Lived',
+    ], $book1->id());
+    $book_manager->updateOutline($page1);
+
+    /** @var \Drupal\node\NodeInterface $book2 */
+    $book2 = $this->createBookPage([
+      'title' => 'Harry Potter and the Deathly Hallows',
+    ]);
+    $book_manager->updateOutline($book2);
+
+    /** @var \Drupal\node\NodeInterface $event */
+    $event = $this->createNode([
+      'type' => 'events',
+    ]);
+
+    $web_assert = $this->assertSession();
+
+    try {
+      $this->visit($path_alias_manager->getAliasByPath("/node/{$book1->id()}"));
+
+      $this->assertNotNull($web_assert->elementExists('css', '.block-book-navigation'));
+      $web_assert->pageTextContains($book1->label());
+      $web_assert->pageTextContains($page1->label());
+      $web_assert->pageTextContains($book2->label());
+
+      $this->visit($path_alias_manager->getAliasByPath("/node/{$event->id()}"));
+      $web_assert->elementNotExists('css', '.block-book-navigation');
+      $web_assert->pageTextNotContains($book1->label());
+      $web_assert->pageTextNotContains($book2->label());
+
+      $this->assertTrue(TRUE);
+    }
+    catch (ExpectationException $e) {
+      $this->fail(sprintf("Test failed: %s\nBacktrace: %s", $e->getMessage(), $e->getTraceAsString()));
+    }
+  }
+
+}
---
+++ profile/tests/src/ExistingSiteJavascript/TestBase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\openscholar\ExistingSiteJavascript;
+
+use Drupal\node\NodeInterface;
+use Drupal\Tests\book\Functional\BookTestTrait;
+use weitzman\DrupalTestTraits\ExistingSiteWebDriverTestBase;
+
+/**
+ * Test base for functional javascript tests.
+ */
+abstract class TestBase extends ExistingSiteWebDriverTestBase {
+
+  use BookTestTrait;
+
+  /**
+   * Creates a book page.
+   *
+   * @param array $values
+   *   (Optional) Initial values. If nothing is passed, the page is created with
+   *   default values.
+   * @param int|null $book_id
+   *   (Optional) Book id where this page should be attached. If nothing is
+   *   passed, this creates a new book.
+   * @param int|null $parent_id
+   *   (Optional) The parent id. If nothing is passed, the page will become the
+   *   first page.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The new entity.
+   */
+  public function createBookPage(array $values = [], $book_id = NULL, $parent_id = NULL) : NodeInterface {
+    /** @var \Drupal\node\NodeInterface $book */
+    $book = $this->createNode($values + [
+      'type' => 'page',
+      'book' => [
+        'bid' => $book_id ?? 'new',
+        'pid' => $parent_id ?? -1,
+        'weight' => 0,
+      ],
+    ]);
+
+    return $book;
+  }
+
+}
