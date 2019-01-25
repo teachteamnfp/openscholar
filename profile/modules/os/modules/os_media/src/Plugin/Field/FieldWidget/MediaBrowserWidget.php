d2da95bee7f9bc8a5da8e19894e3785bdf9626ac
mpetrovic <mpetrovic@guneva.net>
2019-01-25 13:52:02 -0500

    #10961: Progress

--- profile/modules/os/modules/os_media/src/Plugin/Field/FieldWidget/MediaBrowserWidget.php
+++ profile/modules/os/modules/os_media/src/Plugin/Field/FieldWidget/MediaBrowserWidget.php
@@ -1,14 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: New User
- * Date: 1/24/2019
- * Time: 3:19 PM
- */
 
 namespace Drupal\os_media\Plugin\Field\FieldWidget;
 
 
-class MediaBrowserWidget {
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Plugin\Field\FieldWidget\FileWidget;
+
+/**
+ * Class MediaBrowserWidget
+ *
+ * @FieldWidget(
+ *   id = "media_browser_widget",
+ *   label = @Translation("Media Browser"),
+ *   field_types = {
+ *     "entity_reference"
+ *   }
+ * )
+ */
+class MediaBrowserWidget extends FileWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    ksm($element);
+
+    return $element;
+  }
 
 }
