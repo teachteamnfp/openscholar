f383067fd3a18e9c54b92a89fafcc9b84eced31a
mpetrovic <mpetrovic@guneva.net>
2019-01-25 13:51:03 -0500

    resolve merge conflicts

---
+++ profile/modules/os/modules/os_media/src/Plugin/Field/FieldWidget/MediaBrowserWidget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: New User
+ * Date: 1/24/2019
+ * Time: 3:19 PM
+ */
+
+namespace Drupal\os_media\Plugin\Field\FieldWidget;
+
+
+class MediaBrowserWidget {
+
+}
