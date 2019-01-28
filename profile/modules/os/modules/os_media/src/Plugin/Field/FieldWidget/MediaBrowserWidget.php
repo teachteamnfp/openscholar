33f2e693ac6e8c6035b08849e5ef20cbfe4d1ad8
mpetrovic <mpetrovic@guneva.net>
2019-01-28 15:37:32 -0500

    #10961: Placeholder media browser field.

--- profile/modules/os/modules/os_media/src/Plugin/Field/FieldWidget/MediaBrowserWidget.php
+++ profile/modules/os/modules/os_media/src/Plugin/Field/FieldWidget/MediaBrowserWidget.php
@@ -4,8 +4,8 @@ namespace Drupal\os_media\Plugin\Field\FieldWidget;
 
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\file\Plugin\Field\FieldWidget\FileWidget;
 
 /**
  * Class MediaBrowserWidget
@@ -15,18 +15,31 @@ use Drupal\file\Plugin\Field\FieldWidget\FileWidget;
  *   label = @Translation("Media Browser"),
  *   field_types = {
  *     "entity_reference"
- *   }
+ *   },
+ *   multiple_values = TRUE,
  * )
  */
-class MediaBrowserWidget extends FileWidget {
+class MediaBrowserWidget extends WidgetBase {
 
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element = parent::formElement($items, $delta, $element, $form, $form_state);
 
-    ksm($element);
+    $element['#type'] = 'fieldset';
+    $element['media-browser-field'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#attributes' => [
+        'media-browser-field' => '',
+      ],
+      '#markup' => t('Loading the Media Browser. Please wait a moment.'),
+      '#attached' => [
+        'library' => [
+          'os_media/mediaBrowserField'
+        ]
+      ]
+    ];
 
     return $element;
   }
