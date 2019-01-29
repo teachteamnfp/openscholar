aeb791ff14ab91f0d7e91161cd9db6898e834418
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-01-29 16:35:25 +0100

    Issue #11001 - Create basic test for admin cp settings and frontpage source checking

---
+++ profile/modules/custom/os_metatag/tests/src/ExistingSite/HtmlHeadOsMetatagTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\os_metatag\ExistingSite;
+
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Tests os_metatag module.
+ *
+ * @group metatag
+ * @group functional
+ * @covers os_metatag.module
+ */
+class HtmlHeadOsMetatagTest extends ExistingSiteBase {
+
+  /**
+   * Tests os_metatag on empty config not render link rel.
+   */
+  public function testHtmlHeadOnEmptyConfig() {
+    $web_assert = $this->assertSession();
+
+    $this->visit("/");
+    $web_assert->statusCodeEquals(200);
+    $expectedHtmlValue = '<link rel="publisher"';
+    $this->assertNotContains($expectedHtmlValue, $this->getCurrentPageContent(), 'HTML head contains publisher link.');
+    $expectedHtmlValue = '<link rel="author"';
+    $this->assertNotContains($expectedHtmlValue, $this->getCurrentPageContent(), 'HTML head contains author link.');
+  }
+
+}
