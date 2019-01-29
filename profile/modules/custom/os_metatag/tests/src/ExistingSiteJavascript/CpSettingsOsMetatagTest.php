3be90a3cbe8fa204cedb6b6ed8a7f23a8e5b48a4
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-01-29 18:53:24 +0100

    Issue #11001 - Fix test permission

--- profile/modules/custom/os_metatag/tests/src/ExistingSiteJavascript/CpSettingsOsMetatagTest.php
+++ profile/modules/custom/os_metatag/tests/src/ExistingSiteJavascript/CpSettingsOsMetatagTest.php
@@ -26,6 +26,7 @@ class CpSettingsOsMetatagTest extends ExistingSiteWebDriverTestBase {
     parent::setUp();
     $this->adminUser = $this->createUser([
       'access administration pages',
+      'access control panel',
     ]);
   }
 
