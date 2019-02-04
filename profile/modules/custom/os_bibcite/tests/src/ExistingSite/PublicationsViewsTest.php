239662a1aefda5da8a98170cf79f1c43658eed6e
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-04 10:41:31 +0530

    Issue #11011: Publications by type test

---
+++ profile/modules/custom/os_bibcite/tests/src/ExistingSite/PublicationsViewsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\os_bibcite\ExistingSite;
+
+/**
+ * Tests publication views.
+ *
+ * @group vsite
+ * @group kernel
+ */
+class PublicationsViewsTest extends TestBase {
+
+  /**
+   * Tests title display of publications.
+   */
+  public function testType() {
+    $this->createReference([
+      'title' => 'Mona Lisa',
+    ]);
+
+    $this->createReference([
+      'title' => 'The Rust Programming Language',
+      'type' => 'journal',
+      'bibcite_year' => [
+        'value' => 2010,
+      ],
+    ]);
+
+    /** @var array $result */
+    $result = views_get_view_result('publications', 'page_1');
+
+    $this->assertCount(2, $result);
+
+    $grouped_result = [];
+
+    foreach ($result as $item) {
+      $grouped_result[$item->_entity->bundle()][] = $item->_entity->label();
+    }
+
+    $this->assertCount(1, $grouped_result['artwork']);
+    $this->assertCount(1, $grouped_result['journal']);
+  }
+
+}
---
+++ profile/modules/custom/os_bibcite/tests/src/ExistingSite/TestBase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\Tests\os_bibcite\ExistingSite;
+
+use Drupal\bibcite_entity\Entity\Reference;
+use Drupal\bibcite_entity\Entity\ReferenceInterface;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * TestBase for bibcite customizations.
+ */
+abstract class TestBase extends ExistingSiteBase {
+
+  /**
+   * Creates a reference.
+   *
+   * @param array $values
+   *   (Optional) Default values for the reference.
+   *
+   * @return \Drupal\bibcite_entity\Entity\ReferenceInterface
+   *   The new reference entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function createReference(array $values = []) : ReferenceInterface {
+    $reference = Reference::create($values + [
+      'title' => $this->randomString(),
+      'type' => 'artwork',
+      'bibcite_year' => [
+        'value' => 1980,
+      ],
+    ]);
+
+    $reference->save();
+
+    $this->markEntityForCleanup($reference);
+
+    return $reference;
+  }
+
+}
