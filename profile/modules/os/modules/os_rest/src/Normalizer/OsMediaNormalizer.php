00afdd19a20e73afcb7d6fec59bccb0cd072d6ad
mpetrovic <mpetrovic@guneva.net>
2019-02-06 17:04:07 -0500

    #10961: Get alt and title editing working without blowing away the other field.

--- profile/modules/os/modules/os_rest/src/Normalizer/OsMediaNormalizer.php
+++ profile/modules/os/modules/os_rest/src/Normalizer/OsMediaNormalizer.php
@@ -3,8 +3,10 @@
 namespace Drupal\os_rest\Normalizer;
 
 use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Entity\Plugin\DataType\EntityReference;
 use Drupal\Core\Image\Image;
 use Drupal\file\FileInterface;
+use Drupal\media\MediaInterface;
 use Drupal\media\MediaTypeInterface;
 use Drupal\serialization\Normalizer\ContentEntityNormalizer;
 use Drupal\serialization\Normalizer\NormalizerBase;
@@ -63,15 +65,28 @@ class OsMediaNormalizer extends ContentEntityNormalizer {
     return $output;
   }
 
+  public function denormalize($data, $class, $format = NULL, array $context = []) {
+    $entity = parent::denormalize($data, $class, $format, $context);
+
+    if (in_array('alt', $entity->_restSubmittedFields) || in_array('title', $entity->_restSubmittedFields)) {
+      $entity->_restSubmittedFields = array_diff($entity->_restSubmittedFields, ['alt', 'title']);
+      $entity->_restSubmittedFields[] = 'field_media_image';
+    }
+    $entity->restSubmittedFields = array_diff($entity->_restSubmittedFields, ['fid']);
+
+    return $entity;
+  }
+
   /**
    * {@inheritdoc}
    */
   public function denormalizeFieldData(array $data, FieldableEntityInterface $entity, $format, array $context) {
+    $original = \Drupal::routeMatch()->getParameter('media');
 
     if (!empty($data['name'])) {
       $input = [
         'name' => [
-            'value' => $data['name']
+          'value' => $data['name']
         ]
       ];
       $fieldList = $entity->get('name');
@@ -90,35 +105,28 @@ class OsMediaNormalizer extends ContentEntityNormalizer {
       $fieldList->setValue([]);
       $class = get_class($fieldList);
       $context['target_instance'] = $fieldList;
-      $this->serializer->deserialize($input, $class, $format, $context);
+      $this->serializer->deserialize(json_encode($input), $class, $format, $context);
     }
-    if (!empty($data['alt'])) {
-      $input = [
-        'alt' => $data['name']
-      ];
+    if (!empty($data['alt']) || !empty($data['title'])) {
+      $input = $original->get('field_media_image')->get(0)->getValue();
+
+      if (!empty($data['alt'])) {
+        $input['alt'] = $data['alt'];
+      }
+      if (!empty($data['title'])) {
+        $input['title'] = $data['title'];
+      }
+
       $fieldList = $entity->get('field_media_image');
-      $typed = $fieldList->get(0);
-      $typed->applyDefaultValue(false);
-      $class = get_class($typed);
-      $context['target_instance'] = $typed;
-      $this->serializer->deserialize($input, $class, $format, $context);
-    }
-    if (!empty($data['title'])) {
-      $input = [
-        'title' => $data['title']
-      ];
-      $fieldList = $entity->get('field_media_image');
-      $typed = $fieldList->get(0);
-      $typed->applyDefaultValue(false);
-      $class = get_class($typed);
-      $context['target_instance'] = $typed;
-      $this->serializer->deserialize($input, $class, $format, $context);
+      $class = get_class($fieldList);
+      $context['target_instance'] = $fieldList;
+      $this->serializer->deserialize(json_encode([$input]), $class, $format, $context);
     }
     if (!empty($data['description'])) {
       $input = [
-        'field_description' => $data['desscription']
+        'field_description' => $data['description']
       ];
-      $input['field_description'][0]['value'] = $data['description'];
+
       $fieldList = $entity->get('name');
       $fieldList->setValue([]);
       $class = get_class($fieldList);
