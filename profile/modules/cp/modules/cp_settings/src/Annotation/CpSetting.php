e39920b18afdfdada8bce5ee120654e2140507c0
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-08 16:52:10 +0530

    Issue #11062: Trial and error - Fix the problem

--- profile/modules/cp/modules/cp_settings/src/Annotation/CpSetting.php
+++ profile/modules/cp/modules/cp_settings/src/Annotation/CpSetting.php
@@ -5,10 +5,34 @@ namespace Drupal\cp_settings\Annotation;
 use Drupal\Component\Annotation\Plugin;
 
 /**
- * CP settings annotion.
+ * CP settings annotation.
  *
  * @Annotation
  */
 class CpSetting extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The setting title.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $title;
+
+
+  /**
+   * Setting group.
+   *
+   * @var array
+   */
+  public $group;
 
 }
