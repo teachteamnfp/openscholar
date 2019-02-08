b3b686580944965d2f21f273859293fbf741c7d3
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-08 10:05:50 +0530

    Issue #11011: Citation style in publications - init

--- profile/modules/apps/os_publications/src/Plugin/views/field/LabelFirstLetterExclPreposition.php
+++ profile/modules/apps/os_publications/src/Plugin/views/field/LabelFirstLetterExclPreposition.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\os_publications\Plugin\views\field;
 
-use Drupal\os_publications\PublicationsListingHelper;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field handler to display entity label excluding prepositions in beginning.
@@ -12,6 +12,27 @@ use Drupal\views\ResultRow;
  * @ViewsField("os_publications_first_letter_title_excl_prep")
  */
 class LabelFirstLetterExclPreposition extends FieldPluginBase {
+
+  /**
+   * LabelFirstLetterExclPreposition constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return parent::create($container, $configuration, $plugin_id, $plugin_definition);
+  }
 
   /**
    * {@inheritdoc}
@@ -25,7 +46,7 @@ class LabelFirstLetterExclPreposition extends FieldPluginBase {
    */
   public function render(ResultRow $values) {
     /** @var \Drupal\os_publications\PublicationsListingHelperInterface $publications_listing_helper */
-    $publications_listing_helper = new PublicationsListingHelper();
+    $publications_listing_helper = \Drupal::service('os_publications.listing_helper');
     return $publications_listing_helper->convertLabel($this->sanitizeValue($values->_entity->label()));
   }
 
