b6a57188d1c8a6cea492962cfebb66d8b74014d0
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-11 16:04:29 +0530

    Issue #11011: Author last name first letter - with DI

--- profile/modules/apps/os_publications/src/Plugin/views/field/AuthorLastNameFirstLetter.php
+++ profile/modules/apps/os_publications/src/Plugin/views/field/AuthorLastNameFirstLetter.php
@@ -3,8 +3,10 @@
 namespace Drupal\os_publications\Plugin\views\field;
 
 use Drupal\os_publications\PublicationsListingHelper;
+use Drupal\os_publications\PublicationsListingHelperInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field handler to display entity label excluding prepositions in beginning.
@@ -12,6 +14,37 @@ use Drupal\views\ResultRow;
  * @ViewsField("os_publications_first_letter_last_name_author")
  */
 class AuthorLastNameFirstLetter extends FieldPluginBase {
+
+  /**
+   * Publications listing helper.
+   *
+   * @var \Drupal\os_publications\PublicationsListingHelperInterface
+   */
+  protected $publicationsListingHelper;
+
+  /**
+   * AuthorLastNameFirstLetter constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\os_publications\PublicationsListingHelperInterface $publications_listing_helper
+   *   Publications listing helper.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PublicationsListingHelperInterface $publications_listing_helper) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->publicationsListingHelper = $publications_listing_helper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('os_publications.listing_helper'));
+  }
 
   /**
    * {@inheritdoc}
@@ -32,9 +65,7 @@ class AuthorLastNameFirstLetter extends FieldPluginBase {
     $entity_adapter = $entity_reference_item->get('entity')->getTarget();
     /** @var \Drupal\bibcite_entity\Entity\ContributorInterface $bibcite_contributor */
     $bibcite_contributor = $entity_adapter->getValue();
-    /** @var \Drupal\os_publications\PublicationsListingHelperInterface $publications_listing_helper */
-    $publications_listing_helper = new PublicationsListingHelper();
-    return $publications_listing_helper->convertAuthorName($this->sanitizeValue($bibcite_contributor->getLastName()));
+    return $this->publicationsListingHelper->convertAuthorName($this->sanitizeValue($bibcite_contributor->getLastName()));
   }
 
 }
