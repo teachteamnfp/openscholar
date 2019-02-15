26494938814d085e363eede5da7d9e5807bdaf85
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-15 14:10:02 +0530

    Issue #11079: Stricter code standard check
    Motivation:
    
    I have seen that people are loading Drupal services statically, which is
    not a best practice. And the `--warning-severity` flag suppresses that
    warning.
    
    This commit will make sure that the developer has taken care of all
    warnings in the files he has worked on.. so that one day we can remove
    the `--warning-severity` flag from the CI check :)

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running pre-commit hook"
-composer code-standard
+git diff --cached --name-only | xargs composer code-standard $1
 
 # $? stores exit value of the last command
 if [[ $? -ne 0 ]]; then
