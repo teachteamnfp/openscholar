cc6f6a5d4f7dfe9671be6d408ac065de6895c63b
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-18 18:25:28 +0530

    Issue #10916: Use make for building environment

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -173,7 +173,7 @@ class RoboFile extends \Robo\Tasks
 
         $tasks[] = $this->taskExec('docker-compose pull --parallel');
         $tasks[] = $this->taskExec('docker-compose up -d');
-        $tasks[] = $this->taskExec('docker-compose exec -T php composer install');
+        $tasks[] = $this->taskExec('make');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config/phpunit.xml web/core/phpunit.xml');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config/bootstrap.php web/core/tests/bootstrap.php');
         $tasks[] = $this->taskExec('docker-compose exec -T php mkdir web/sites/simpletest');
