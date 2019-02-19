68d7ef2bf7695ad658a12c52d74eb99e94d2f633
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-02-19 16:15:56 +0100

    Issue #11019 - Implement image media view layer

--- profile/modules/apps/os_widgets/src/BlockContentType/EmbedMediaWidget.php
+++ profile/modules/apps/os_widgets/src/BlockContentType/EmbedMediaWidget.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\os_widgets\BlockContentType;
 
+use Drupal\file\Entity\File;
+
 /**
- * Class FeaturedPostsWidget.
+ * Class EmbedMediaWidget.
  */
 class EmbedMediaWidget implements BlockContentTypeInterface {
 
@@ -15,14 +17,32 @@ class EmbedMediaWidget implements BlockContentTypeInterface {
       return $variables;
     }
     $field_max_width_values = $block_content->get('field_max_width')->getValue();
-    $max_width = $field_max_width_values[0]['value'];
-    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-    /** @var \Drupal\Core\Field\EntityReferenceFieldItemList $fieldItemsList */
-    $media_select_list = $variables['content']['field_media_select']['#items'];
+    $max_width = $field_max_width_values[0]['value'] ?? 0;
+    $media_select_list = $block_content->get('field_media_select');
     $referenced_entities = $media_select_list->referencedEntities();
 
-    foreach ($referenced_entities as $delta => $node) {
-      // TODO: continue implement view layer.
+    foreach ($referenced_entities as $delta => $media) {
+      $bundle = $media->bundle();
+      switch ($bundle) {
+        case 'image':
+          $field_values = $media->get('field_media_image')->getValue();
+          $file_id = $field_values[0]['target_id'];
+          $file = File::load($file_id);
+          $uri = $file->getFileUri();
+          $embed_media = [
+            '#theme' => 'image',
+            '#uri' => $uri,
+            '#alt' => $field_values[0]['alt'],
+            '#title' => $field_values[0]['title'],
+            '#width' => $max_width,
+          ];
+          $variables['content']['embed_media'] = $embed_media;
+          break;
+
+        case 'video':
+          $field_values = $media->get('field_media_oembed_video');
+          break;
+      }
     }
     return $variables;
   }
