081f5d19912bb1ebf836297467311df9a60ab471
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-19 11:54:33 +0530

    Revert "Issue #10916: Trial and error - Check if npm container running"
    This reverts commit 2e4fb60b303acaffb36b5b4cae487d4d0f33d9af.

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -173,7 +173,6 @@ class RoboFile extends \Robo\Tasks
 
         $tasks[] = $this->taskExec('docker-compose pull --parallel');
         $tasks[] = $this->taskExec('docker-compose up -d');
-        $tasks[] = $this->taskExec('docker ps');
         $tasks[] = $this->taskExec('make');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config/phpunit.xml web/core/phpunit.xml');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config/bootstrap.php web/core/tests/bootstrap.php');
