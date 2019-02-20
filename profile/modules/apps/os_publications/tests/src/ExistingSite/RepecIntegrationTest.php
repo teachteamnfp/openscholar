3c53ecfb058f007430e10d9a8695503442284bf1
Subhojit Paul <subhojitpaul21@gmail.com>
2019-02-20 18:17:11 +0530

    Issue #11011: Is entity shareable repec setting test

--- profile/modules/apps/os_publications/tests/src/ExistingSite/RepecIntegrationTest.php
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/RepecIntegrationTest.php
@@ -17,12 +17,30 @@ class RepecIntegrationTest extends TestBase {
   protected $repec;
 
   /**
+   * Config service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+  /**
+   * Default repec bundle settings.
+   *
+   * @var array
+   */
+  protected $defaultRepecBundleSettings;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
 
     $this->repec = $this->container->get('repec');
+    $this->configFactory = $this->container->get('config.factory');
+    /** @var \Drupal\Core\Config\ImmutableConfig $default_repec_settings */
+    $default_repec_settings = $this->configFactory->get('repec.settings');
+    $this->defaultRepecBundleSettings = $default_repec_settings->get('repec_bundle');
   }
 
   /**
@@ -54,12 +72,43 @@ class RepecIntegrationTest extends TestBase {
    *
    * @covers \Drupal\repec\Repec::isEntityShareable
    * @covers \Drupal\repec\Repec::createPaperTemplate
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function testEntityShareable() {
-    // Create reference with "is shared" setting off.
-    // Assert presence of rdf file in public://
-    // Create reference with "is shared" setting on.
-    // Assert not presence of rdf file in public://.
+    $reference = $this->createReference();
+    $serie_directory_config = $this->repec->getEntityBundleSettings('serie_directory', $reference->getEntityTypeId(), $reference->bundle());
+    $directory = "{$this->repec->getArchiveDirectory()}{$serie_directory_config}/";
+    $file_name = "{$serie_directory_config}_{$reference->getEntityTypeId()}_{$reference->id()}.rdf";
+    $this->assertFileExists("$directory/$file_name");
+
+    /** @var \Drupal\Core\Config\Config $repec_settings_mut */
+    $repec_settings_mut = $this->configFactory->getEditable('repec.settings');
+    /** @var array $repec_bundle_settings */
+    $repec_bundle_settings = $repec_settings_mut->get('repec_bundle');
+    $repec_bundle_settings['bibcite_reference']['artwork'] = 'a:11:{s:7:"enabled";i:1;s:10:"serie_type";s:6:"wpaper";s:10:"serie_name";s:7:"artwork";s:15:"serie_directory";s:6:"wpaper";s:17:"restriction_field";s:15:"field_is_sticky";s:11:"author_name";s:6:"author";s:8:"abstract";s:14:"bibcite_abst_e";s:13:"creation_date";s:7:"created";s:8:"file_url";s:11:"bibcite_url";s:8:"keywords";s:8:"keywords";s:20:"restriction_by_field";i:1;}';
+    $repec_settings_mut->set('repec_bundle', $repec_bundle_settings);
+    $repec_settings_mut->save();
+
+    $reference = $this->createReference([
+      'field_is_sticky' => [
+        'value' => FALSE,
+      ],
+    ]);
+    $file_name = "{$serie_directory_config}_{$reference->getEntityTypeId()}_{$reference->id()}.rdf";
+    $this->assertFileNotExists("$directory/$file_name");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    /** @var \Drupal\Core\Config\Config $repec_settings_mut */
+    $repec_settings_mut = $this->configFactory->getEditable('repec.settings');
+    $repec_settings_mut->set('repec_bundle', $this->defaultRepecBundleSettings);
+    $repec_settings_mut->save();
+
+    parent::tearDown();
   }
 
 }
