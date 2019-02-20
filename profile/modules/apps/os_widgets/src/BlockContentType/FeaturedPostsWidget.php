dd6985bad21d0a59dc93522f9e3add356468af5e
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-02-20 15:09:23 +0100

    Issue #11019 - Coding standards fix, minimal refactor

--- profile/modules/apps/os_widgets/src/BlockContentType/FeaturedPostsWidget.php
+++ profile/modules/apps/os_widgets/src/BlockContentType/FeaturedPostsWidget.php
@@ -10,40 +10,40 @@ class FeaturedPostsWidget implements BlockContentTypeInterface {
   /**
    * {@inheritdoc}
    */
-  public function buildBlock($variables, $blockContent) {
-    if (empty($blockContent)) {
+  public function buildBlock($variables, $block_content) {
+    if (empty($block_content)) {
       return $variables;
     }
-    $displayStyleValues = $blockContent->get('field_display_style')->getValue();
+    $displayStyleValues = $block_content->get('field_display_style')->getValue();
     $displayStyle = $displayStyleValues[0]['value'];
-    $viewBuilder = \Drupal::entityTypeManager()->getViewBuilder('node');
-    /** @var \Drupal\Core\Field\EntityReferenceFieldItemList $fieldItemsList */
-    $fieldItemsList = $variables['content']['field_featured_posts']['#items'];
-    if (empty($fieldItemsList)) {
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    /** @var \Drupal\Core\Field\FieldItemList $media_select_list */
+    $featured_posts_list = $block_content->get('field_featured_posts');
+    if (empty($featured_posts_list)) {
       return $variables;
     }
-    $referencedEntities = $fieldItemsList->referencedEntities();
-    $hideTitleValues = $blockContent->get('field_hide_title')->getValue();
+    $referenced_entities = $featured_posts_list->referencedEntities();
+    $hide_title_values = $block_content->get('field_hide_title')->getValue();
     if ($displayStyle != 'title') {
       /** @var \Drupal\node\Entity\Node $node */
-      foreach ($referencedEntities as $delta => $node) {
-        $build = $viewBuilder->view($node, $displayStyle);
-        $build['os_widgets_hide_node_title'] = !empty($hideTitleValues[0]['value']) ? TRUE : FALSE;
+      foreach ($referenced_entities as $delta => $node) {
+        $build = $view_builder->view($node, $displayStyle);
+        $build['os_widgets_hide_node_title'] = !empty($hide_title_values[0]['value']) ? TRUE : FALSE;
         $variables['content']['field_featured_posts'][$delta] = $build;
       }
     }
-    $isRandomValues = $blockContent->get('field_is_random')->getValue();
-    if (!empty($isRandomValues[0]['value'])) {
-      $displayedDelta = array_rand($referencedEntities);
-      foreach ($referencedEntities as $delta => $node) {
+    $is_random_values = $block_content->get('field_is_random')->getValue();
+    if (!empty($is_random_values[0]['value'])) {
+      $displayedDelta = array_rand($referenced_entities);
+      foreach ($referenced_entities as $delta => $node) {
         if ($displayedDelta != $delta) {
           // Hide other referenced entity.
           $variables['content']['field_featured_posts'][$delta]['#access'] = FALSE;
         }
       }
     }
-    $isStyledValues = $blockContent->get('field_is_styled')->getValue();
-    if (!empty($isStyledValues[0]['value'])) {
+    $is_styled_values = $block_content->get('field_is_styled')->getValue();
+    if (!empty($is_styled_values[0]['value'])) {
       $variables['attributes']['class'][] = 'styled';
     }
     return $variables;
