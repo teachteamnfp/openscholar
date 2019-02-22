f5c46240e8871ea53852e35c16e760b6857ec51f
mpetrovic <mpetrovic@guneva.net>
2019-02-22 11:43:45 -0500

    #10961: Use drupalSettings to set the purl when making AJAX requests.

--- profile/modules/vsite/js/vsite.ajaxContext.js
+++ profile/modules/vsite/js/vsite.ajaxContext.js
@@ -5,17 +5,20 @@
 
   Drupal.Ajax.prototype.beforeSend = function (xmlhttprequest, options) {
     origBeforeSend.call(this, xmlhttprequest, options);
+    // skip if we're not on a space
+    if (drupalSettings.spaces != undefined) {
 
-    // change the url
-    var url = options.url,
-      basePath = drupalSettings.path.baseUrl,
-      vsite = 'site01',
-      pattern = new RegExp('^'+basePath.replace('/', '\\/')),
-      dupePattern = new RegExp('^'+basePath.replace('/', '\\/')+vsite);
+      // change the url
+      var url = options.url,
+        basePath = drupalSettings.path.baseUrl,
+        vsite = drupalSettings.spaces.purl,
+        pattern = new RegExp('^' + basePath.replace('/', '\\/')),
+        dupePattern = new RegExp('^' + basePath.replace('/', '\\/') + vsite);
 
-    if (!url.match(dupePattern)) {
-      url = url.replace(pattern, basePath + vsite + '/');
-      options.url = url;
+      if (!url.match(dupePattern)) {
+        url = url.replace(pattern, basePath + vsite + '/');
+        options.url = url;
+      }
     }
   }
 
