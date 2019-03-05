224bc3dfe19fd495bfbd67d04e4f3181965723e1
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-03-05 11:18:13 +0530

    #11110: added code to gulp file so it will create css folder in each child themes and build css

--- profile/themes/gulpfile.js
+++ profile/themes/gulpfile.js
@@ -4,7 +4,9 @@
 var gulp = require('gulp'),
 	eslint = require('gulp-eslint'),
   sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps')
+  sourcemaps = require('gulp-sourcemaps'),
+  rename = require('gulp-rename'),
+  path = require('path');
 	//autoprefixer = require('gulp-autoprefixer'),
 	//imagemin = require('gulp-imagemin');
 
@@ -16,11 +18,14 @@ var gulp = require('gulp'),
         outputStyle: 'uncompressed'
       }).on('error', sass.logError))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./css'));
+      .pipe(rename(function (file) {
+        // file.dirname = current folder, your "scss"
+        // then get the parent of the current folder, e.g., "themename1", "themename2", etc.
+        let parentFolder = path.dirname(file.dirname)
+        // set each file's folder to "themename1/css", "themename2/css", etc.
+        file.dirname = path.join(parentFolder, 'css');
+      }))
+      .pipe(gulp.dest('.'));
   });
-
-  gulp.task('watch', gulp.series('sass', function () {
-    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
-  }));
 
 })();
