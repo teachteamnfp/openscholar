349bf60c4ec8ed5574c0776a5901e9ce462d326f
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-03-06 14:12:10 +0100

    Issue #11062 - Remove other test cache clear

--- profile/modules/custom/os_mailchimp/tests/src/ExistingSiteJavascript/CpSettingsOsMailChimpTest.php
+++ profile/modules/custom/os_mailchimp/tests/src/ExistingSiteJavascript/CpSettingsOsMailChimpTest.php
@@ -38,9 +38,6 @@ class CpSettingsOsMailChimpTest extends ExistingSiteWebDriverTestBase {
     $this->drupalLogin($this->adminUser);
 
     $this->visit("/cp/settings/mailchimp");
-    $web_assert->statusCodeEquals(403);
-    drupal_flush_all_caches();
-    $this->visit("/cp/settings/mailchimp");
     $web_assert->statusCodeEquals(200);
 
     $edit = [
