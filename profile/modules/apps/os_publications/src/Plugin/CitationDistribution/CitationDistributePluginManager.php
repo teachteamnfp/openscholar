9eb7a621f11b82a9d2b14506b8c7e6f54c80482e
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-06 16:40:29 +0530

    Issue #11011: Removed other global functions
    Figured that they are not yet fully implemented. For demo purpose, this
    can be skipped, and later implement completely.

--- profile/modules/apps/os_publications/src/Plugin/CitationDistribution/CitationDistributePluginManager.php
+++ profile/modules/apps/os_publications/src/Plugin/CitationDistribution/CitationDistributePluginManager.php
@@ -3,6 +3,8 @@
 namespace Drupal\os_publications\Plugin\CitationDistribution;
 
 use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
 
@@ -12,9 +14,16 @@ use Drupal\Core\Plugin\DefaultPluginManager;
 class CitationDistributePluginManager extends DefaultPluginManager {
 
   /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cacheBackend, ModuleHandlerInterface $module_handler) {
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cacheBackend, ModuleHandlerInterface $module_handler, ConfigFactory $config_factory) {
     parent::__construct(
       'Plugin/CitationDistribution',
       $namespaces,
@@ -25,6 +34,35 @@ class CitationDistributePluginManager extends DefaultPluginManager {
 
     $this->alterInfo('citation_distribute');
     $this->setCacheBackend($cacheBackend, 'citation_distribute_plugins');
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Distributes the entity in repositories.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity.
+   */
+  public function distribute(EntityInterface $entity) {
+    /** @var \Drupal\options\Plugin\Field\FieldType\ListStringItem $item */
+    foreach ($entity->get('distribution') as $item) {
+      $dist = FALSE;
+      $dist_mode = $this->configFactory->get('citation_distribute.settings')->get('citation_distribute_module_mode');
+      $item->getValue()['value'];
+
+      // TODO make sure entity is published first.
+      if ($dist_mode == 'per_submission') {
+        // TODO: Implement.
+      }
+      else {
+        // Assume batch mode.
+        $dist = TRUE;
+      }
+
+      if ($dist) {
+        // TODO Implement Queue Api for adding entity for cron operation.
+      }
+    }
   }
 
 }
