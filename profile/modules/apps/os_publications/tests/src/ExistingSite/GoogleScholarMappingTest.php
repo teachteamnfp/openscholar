ad3e75ba46c267c6813ad429f9719e5e6c3695cb
uttkarsh-26 <uttkarsh.26@gmail.com>
2019-03-07 18:18:07 +0530

    MetaData test updated and added kernel test for mapping

---
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/GoogleScholarMappingTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\Tests\os_publications\ExistingSite;
+
+/**
+ * Class PublicationsFormTest.
+ *
+ * @group kernel
+ */
+class GoogleScholarMappingTest extends TestBase {
+
+  /**
+   * Admin user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $admin;
+
+  /**
+   * Normal user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->admin = $this->createUser([], '', TRUE);
+    $this->user = $this->createUser();
+  }
+
+  /**
+   * Test Setting form route.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testPublicationSettingsPath() {
+    $this->drupalLogin($this->admin);
+    $reference = $this->createReference();
+
+    $this->drupalGet('bibcite/reference/' . $reference->id());
+    drupal_flush_all_caches();
+    $this->drupalGet('bibcite/reference/' . $reference->id());
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+  /**
+   * Test Metadata Mapping on entity page.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testGoogleScholarMetadataMapping() {
+    $this->drupalLogin($this->admin);
+    $reference = $this->createReference([
+      'title' => 'Google Scholar Mapping test',
+      'bibcite_year' => '2019',
+      'bibcite_abst_e' => 'This is a test for Google Scholar test.',
+      'bibcite_publisher' => 'testpublisher',
+      'distribution' => [
+        'citation_distribute_googlescholar',
+      ],
+    ]);
+    $this->drupalGet('bibcite/reference/' . $reference->id());
+
+    $this->assertSession()->responseContains('name="citation_title" content="Google Scholar Mapping test"');
+    $this->assertSession()->responseContains('name="citation_year" content="2019"');
+    $this->assertSession()->responseContains('name="citation_abstract" content="This is a test for Google Scholar test."');
+    $this->assertSession()->responseContains('name="citation_publisher" content="testpublisher"');
+
+  }
+
+}
