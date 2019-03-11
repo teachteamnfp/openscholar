7f91b542e2af1df987549a48387360e4afa2522f
mpetrovic <mpetrovic@guneva.net>
2019-03-11 11:12:29 -0400

    #11123: Some basics

--- profile/modules/cp/modules/cp_users/src/Controller/CpUserMainController.php
+++ profile/modules/cp/modules/cp_users/src/Controller/CpUserMainController.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: New User
- * Date: 3/8/2019
- * Time: 4:53 PM
- */
 
 namespace Drupal\cp_users\Controller;
 
 
-class CpUserMainController {
+use Drupal\Core\Controller\ControllerBase;
+
+class CpUserMainController extends ControllerBase {
 
 }
