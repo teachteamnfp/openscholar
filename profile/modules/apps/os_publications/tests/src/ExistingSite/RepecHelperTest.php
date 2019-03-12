de1325fa7d69762ab4d2fa7c69b44d15b16c2c14
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-12 11:06:05 +0530

    Issue #11130: getKeywords test

--- profile/modules/apps/os_publications/tests/src/ExistingSite/RepecHelperTest.php
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/RepecHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\os_publications\ExistingSite;
 
+use Drupal\bibcite_entity\Entity\KeywordInterface;
 use Drupal\os_publications\RepecHelper;
 
 /**
@@ -36,6 +37,42 @@ class RepecHelperTest extends TestBase {
 
     $repec_helper = new RepecHelper($reference_with_contributor);
     $this->assertEquals($contributor->id(), $repec_helper->getContributor()->id());
+  }
+
+  /**
+   * Tests getKeywords.
+   *
+   * @covers ::getKeywords
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testGetKeywords() {
+    $keyword1 = $this->createKeyword();
+    $keyword2 = $this->createKeyword();
+
+    $reference_wo_keywords = $this->createReference();
+    $reference_with_keywords = $this->createReference([
+      'keywords' => [
+        [
+          'target_id' => $keyword1->id(),
+        ],
+        [
+          'target_id' => $keyword2->id(),
+        ],
+      ],
+    ]);
+
+    $repec_helper = new RepecHelper($reference_wo_keywords);
+    $this->assertNull($repec_helper->getKeywords());
+
+    $repec_helper = new RepecHelper($reference_with_keywords);
+    $this->assertCount(2, $repec_helper->getKeywords());
+
+    $keyword_ids = array_map(function (KeywordInterface $keyword) {
+      return $keyword->id();
+    }, $repec_helper->getKeywords());
+    $this->assertTrue(in_array($keyword1->id(), $keyword_ids));
+    $this->assertTrue(in_array($keyword2->id(), $keyword_ids));
   }
 
 }
