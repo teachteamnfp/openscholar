2a00d646bb89ddffb090d66b571b5cc68ac9fcef
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-12 13:00:44 +0530

    Issue #11130: journl template test

--- profile/modules/apps/os_publications/tests/src/ExistingSite/RepecIntegrationTest.php
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/RepecIntegrationTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\os_publications\ExistingSite;
 
+use Drupal\bibcite_entity\Entity\KeywordInterface;
+use Drupal\file\Entity\File;
+
 /**
  * RepecIntegrationTest.
  *
@@ -67,6 +70,7 @@ class RepecIntegrationTest extends TestBase {
     $this->assertContains("Title: {$reference->label()}", $content);
     $this->assertContains("Number: {$reference->uuid()}", $content);
     $this->assertContains("Handle: RePEc:{$this->defaultRepecSettings['archive_code']}:{$this->repec->getEntityBundleSettings('serie_type', $reference->getEntityTypeId(), $reference->bundle())}:{$reference->id()}", $content);
+    $this->assertContains('Template-Type: ReDIF-Paper 1.0', $content);
 
     $reference->delete();
     $this->assertFileNotExists("$directory/$file_name");
@@ -102,6 +106,96 @@ class RepecIntegrationTest extends TestBase {
     ]);
     $file_name = "{$serie_directory_config}_{$reference->getEntityTypeId()}_{$reference->id()}.rdf";
     $this->assertFileNotExists("$directory/$file_name");
+  }
+
+  /**
+   * Tests journl template.
+   *
+   * @covers ::os_publications_repec_template_alter
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testJournlTemplate() {
+    // Make sure journal is configured with correct settings.
+    /** @var \Drupal\Core\Config\Config $repec_settings_mut */
+    $repec_settings_mut = $this->configFactory->getEditable('repec.settings');
+    /** @var array $repec_bundle_settings */
+    $repec_bundle_settings = $repec_settings_mut->get('repec_bundle');
+    $repec_bundle_settings['bibcite_reference']['journal'] = 'a:10:{s:7:"enabled";i:1;s:10:"serie_type";s:6:"journl";s:10:"serie_name";s:6:"journl";s:15:"serie_directory";s:6:"journl";s:17:"restriction_field";s:9:"is_sticky";s:11:"author_name";s:6:"author";s:8:"abstract";s:14:"bibcite_abst_e";s:13:"creation_date";s:7:"created";s:8:"file_url";s:11:"field_files";s:8:"keywords";s:8:"keywords";}';
+    $repec_settings_mut->set('repec_bundle', $repec_bundle_settings);
+    $repec_settings_mut->save();
+
+    file_put_contents('public://example-1.txt', $this->randomMachineName());
+    $file_1 = File::create([
+      'uri' => 'public://example-1.txt',
+    ]);
+    $file_1->save();
+    $file_1_url = file_create_url($file_1->getFileUri());
+    $file_1_type_in_template = ucfirst($file_1->getMimeType());
+    file_put_contents('public://example-2.txt', $this->randomMachineName());
+    $file_2 = File::create([
+      'uri' => 'public://example-2.txt',
+    ]);
+    $file_2->save();
+    $file_2_url = file_create_url($file_2->getFileUri());
+    $file_2_type_in_template = ucfirst($file_2->getMimeType());
+
+    $keyword1 = $this->createKeyword();
+    $keyword2 = $this->createKeyword();
+    $keyword_names = array_map(function (KeywordInterface $keyword) {
+      return $keyword->getName();
+    }, [$keyword1, $keyword2]);
+    $keyword_names_in_template = implode(', ', $keyword_names);
+
+    $contributor = $this->createContributor();
+
+    $abstract = $this->randomString();
+
+    $reference = $this->createReference([
+      'keywords' => [
+        [
+          'target_id' => $keyword1->id(),
+        ],
+        [
+          'target_id' => $keyword2->id(),
+        ],
+      ],
+      'field_files' => [
+        [
+          'target_id' => $file_1->id(),
+        ],
+        [
+          'target_id' => $file_2->id(),
+        ],
+      ],
+      'author' => [
+        [
+          'target_id' => $contributor->id(),
+        ],
+      ],
+      'bibcite_abst_e' => [
+        'value' => $abstract,
+      ],
+    ]);
+
+    $serie_directory_config = $this->repec->getEntityBundleSettings('serie_directory', $reference->getEntityTypeId(), $reference->bundle());
+    $directory = "{$this->repec->getArchiveDirectory()}{$serie_directory_config}/";
+    $file_name = "{$serie_directory_config}_{$reference->getEntityTypeId()}_{$reference->id()}.rdf";
+    $this->assertFileExists("$directory/$file_name");
+
+    $content = file_get_contents("$directory/$file_name");
+
+    $this->assertContains("Title: {$reference->label()}", $content);
+    $this->assertContains("Number: {$reference->uuid()}", $content);
+    $this->assertContains("Handle: RePEc:{$this->defaultRepecSettings['archive_code']}:{$this->repec->getEntityBundleSettings('serie_type', $reference->getEntityTypeId(), $reference->bundle())}:{$reference->id()}", $content);
+    $this->assertContains('Template-Type: ReDIF-Paper 1.0', $content);
+    $this->assertContains("Keywords: {$keyword_names_in_template}", $content);
+    $this->assertContains("File-URL: {$file_1_url}", $content);
+    $this->assertContains("File-Format: {$file_1_type_in_template}", $content);
+    $this->assertContains("File-URL: {$file_2_url}", $content);
+    $this->assertContains("File-Format: {$file_2_type_in_template}", $content);
+    $this->assertContains("Author-Name: {$contributor->getName()}", $content);
+    $this->assertContains("Abstract: {$abstract}", $content);
   }
 
   /**
