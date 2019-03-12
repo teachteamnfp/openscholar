42552d0b040ad17a00f920c436e9684b82397fe1
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-03-12 12:33:26 +0100

    Issue #11000 - Implement redirect add Form

--- profile/modules/custom/os_redirect/src/Form/OsRedirectForm.php
+++ profile/modules/custom/os_redirect/src/Form/OsRedirectForm.php
@@ -5,9 +5,9 @@ namespace Drupal\os_redirect\Form;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\redirect\Entity\Redirect;
 use Drupal\redirect\Form\RedirectForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -21,8 +21,8 @@ class OsRedirectForm extends RedirectForm {
   /**
    * {@inheritdoc}
    */
-  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, ModuleHandlerInterface $moduleHandler) {
-    $this->setEntity(new Redirect([], 'redirect'));
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, ModuleHandlerInterface $moduleHandler, EntityTypeManagerInterface $entity_type_manager) {
+    $this->setEntity($entity_type_manager->getStorage('redirect')->create());
     $this->setModuleHandler($moduleHandler);
     parent::__construct($entity_repository, $entity_type_bundle_info, $time);
   }
@@ -35,7 +35,8 @@ class OsRedirectForm extends RedirectForm {
       $container->get('entity.repository'),
       $container->get('entity_type.bundle.info'),
       $container->get('datetime.time'),
-      $container->get('module_handler')
+      $container->get('module_handler'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -49,6 +50,28 @@ class OsRedirectForm extends RedirectForm {
     $form['language']['widget']['#access'] = FALSE;
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\vsite\Plugin\VsiteContextManagerInterface $vsiteContext */
+    $vsiteContext = \Drupal::service('vsite.context_manager');
+    if ($purl = $vsiteContext->getActivePurl()) {
+      $source = $form_state->getValue(['redirect_source', 0]);
+      $form_state->setValue('redirect_source', [['path' => $purl . '/' . $source['path']]]);
+    }
+    parent::validateForm($form, $form_state);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    parent::save($form, $form_state);
+    $form_state->setRedirect('os_redirect.list');
   }
 
 }
