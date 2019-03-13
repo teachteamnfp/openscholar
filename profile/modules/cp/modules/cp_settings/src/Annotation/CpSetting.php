9697b90c6fe848a441af1a622f47d317cee011ae
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-13 17:38:50 +0530

    Revert "Issue #11062: Trial and error - Fix the problem"
    This reverts commit e39920b18afdfdada8bce5ee120654e2140507c0.

--- profile/modules/cp/modules/cp_settings/src/Annotation/CpSetting.php
+++ profile/modules/cp/modules/cp_settings/src/Annotation/CpSetting.php
@@ -5,35 +5,10 @@ namespace Drupal\cp_settings\Annotation;
 use Drupal\Component\Annotation\Plugin;
 
 /**
- * CP settings annotation.
- *
- * @ingroup plugin_api
+ * CP settings annotion.
  *
  * @Annotation
  */
 class CpSetting extends Plugin {
-
-  /**
-   * The plugin ID.
-   *
-   * @var string
-   */
-  public $id;
-
-  /**
-   * The setting title.
-   *
-   * @var \Drupal\Core\Annotation\Translation
-   *
-   * @ingroup plugin_translatable
-   */
-  public $title;
-
-  /**
-   * Setting group.
-   *
-   * @var \Drupal\cp_settings\Annotation\Group
-   */
-  public $group;
 
 }
