c3edc198b94e098fd9b4a5f2f7a6a99a704c2d5c
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-19 11:19:06 +0530

    Revert "Issue #11216: Debug rebuilded project"
    This reverts commit 91a81651c325c3c3697a32311c83d51626955bb8.

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -92,10 +92,7 @@ if [[ $FORCE_REBUILD == "1" ]] || [[ "$(cmp -b 'openscholar/composer.json' '/tmp
   composer install --ignore-platform-reqs
 
   # Build CSS
-  npm install
-  ls -al
-  ls -al node_modules
-  cd profile/themes/os_base && ./../../../node_modules/.bin/gulp sass
+  npm install && cd profile/themes/os_base && ./../../../node_modules/.bin/gulp sass
 
   cd ../../../..
 
