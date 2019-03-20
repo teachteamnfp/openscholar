87913b624990d0287606a584ce86bbf8e562c562
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-20 13:23:41 +0530

    Revert "Issue #11216: Undoing changes as they are not working"
    This reverts commit 35aa241bc13ba011e9fa7a7fddc58159a70931ed.

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -37,6 +37,7 @@ fi
 # Basic setups
 phpenv local 7.2
 php -v || exit 1
+npm config set cache "${HOME}/cache/npm/"
 
 # Build this branch and push it to Amazon
 # Set up global configuration and install tools needed to build
@@ -90,6 +91,7 @@ if [[ $FORCE_REBUILD == "1" ]] || [[ "$(cmp -b 'openscholar/composer.json' '/tmp
   # Download composer components
   composer install --ignore-platform-reqs || exit 1
 
+  ls -al "$HOME/cache"
   # Do not use codeship cache, and reinstall node modules
   rm -rf node_modules
   npm install || exit 1
