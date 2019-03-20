6e7feec7e92c1f808e92e87f912693a5fe3b2718
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-20 13:35:33 +0530

    Issue #11216: Remove debugging code

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -37,8 +37,6 @@ fi
 # Basic setups
 phpenv local 7.2
 php -v || exit 1
-npm config set cache "${HOME}/cache/npm/"
-npm config list
 
 # Build this branch and push it to Amazon
 # Set up global configuration and install tools needed to build
@@ -92,9 +90,7 @@ if [[ $FORCE_REBUILD == "1" ]] || [[ "$(cmp -b 'openscholar/composer.json' '/tmp
   # Download composer components
   composer install --ignore-platform-reqs || exit 1
 
-  ls -al
-  ls -al "$HOME/cache"
-  # Do not use codeship cache, and reinstall node modules
+  # Do not use the node_modules symlink, and reinstall node modules
   rm -rf node_modules
   npm install || exit 1
 
