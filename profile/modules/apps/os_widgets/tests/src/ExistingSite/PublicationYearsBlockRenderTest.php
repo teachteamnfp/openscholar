01b213eaa2ffb6a15368735ba75ed892437d6d8f
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-03-29 16:53:15 +0100

    Issue #11025 - Write test for years widget and fix views settings

---
+++ profile/modules/apps/os_widgets/tests/src/ExistingSite/PublicationYearsBlockRenderTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\Tests\os_widgets\ExistingSite;
+
+use Drupal\bibcite_entity\Entity\Reference;
+use Drupal\bibcite_entity\Entity\ReferenceInterface;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Class PublicationYearsWidget.
+ *
+ * @group kernel
+ * @group widgets
+ * @covers \Drupal\os_widgets\Plugin\OsWidgets\PublicationYearsWidget
+ */
+class PublicationYearsBlockRenderTest extends OsWidgetsExistingSiteTestBase {
+
+  /**
+   * The object we're testing.
+   *
+   * @var \Drupal\os_widgets\Plugin\OsWidgets\PublicationYearsWidget
+   */
+  protected $publicationYearsWidget;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    Cache::invalidateTags(['config:views.view.publication_years']);
+    $this->publicationYearsWidget = $this->osWidgets->createInstance('publication_years_widget');
+  }
+
+  /**
+   * Test basic listing test without count.
+   */
+  public function testBuildListingYearsWithoutCount() {
+    $this->createReference([
+      'title' => 'Ref 2001',
+      'bibcite_year' => [
+        'value' => 2001,
+      ],
+    ]);
+    $this->createReference([
+      'title' => 'Ref 2010',
+      'bibcite_year' => [
+        'value' => 2010,
+      ],
+    ]);
+
+    $block_content = $this->createBlockContent([
+      'type' => 'publication_years',
+      'field_display_count' => [
+        FALSE,
+      ],
+    ]);
+    $view_builder = $this->entityTypeManager
+      ->getViewBuilder('block_content');
+    $render = $view_builder->view($block_content);
+    $renderer = $this->container->get('renderer');
+
+    /** @var \Drupal\Core\Render\Markup $markup_array */
+    $markup = $renderer->renderRoot($render);
+    $this->assertContains('<a href="/publications/year/2001">2001</a>', $markup->__toString());
+    $this->assertContains('<a href="/publications/year/2010">2010</a>', $markup->__toString());
+    $this->assertNotContains('views-field-display-count', $markup->__toString());
+  }
+
+  /**
+   * Test basic listing test with count.
+   */
+  public function testBuildListingYearsWithCount() {
+    $this->createReference([
+      'title' => 'Ref 2001',
+      'bibcite_year' => [
+        'value' => 2001,
+      ],
+    ]);
+    $this->createReference([
+      'title' => 'Ref 2 2001',
+      'bibcite_year' => [
+        'value' => 2001,
+      ],
+    ]);
+    $this->createReference([
+      'title' => 'Ref 2010',
+      'bibcite_year' => [
+        'value' => 2010,
+      ],
+    ]);
+
+    $block_content = $this->createBlockContent([
+      'type' => 'publication_years',
+      'field_display_count' => [
+        TRUE,
+      ],
+    ]);
+    $view_builder = $this->entityTypeManager
+      ->getViewBuilder('block_content');
+    $render = $view_builder->view($block_content);
+    $renderer = $this->container->get('renderer');
+
+    /** @var \Drupal\Core\Render\Markup $markup_array */
+    $markup = $renderer->renderRoot($render);
+    $this->assertContains('<div class="views-row"><div class="views-field views-field-bibcite-year"><span class="field-content"><a href="/publications/year/2001">2001</a></span></div><div class="views-field views-field-id"><span class="field-content views-field-display-count">(2)</span></div></div>', $markup->__toString());
+    $this->assertContains('<div class="views-row"><div class="views-field views-field-bibcite-year"><span class="field-content"><a href="/publications/year/2010">2010</a></span></div><div class="views-field views-field-id"><span class="field-content views-field-display-count">(1)</span></div></div>', $markup->__toString());
+  }
+
+  /**
+   * Creates a reference.
+   *
+   * @param array $values
+   *   (Optional) Default values for the reference.
+   *
+   * @return \Drupal\bibcite_entity\Entity\ReferenceInterface
+   *   The new reference entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function createReference(array $values = []) : ReferenceInterface {
+    $reference = Reference::create($values + [
+      'title' => $this->randomString(),
+      'type' => 'artwork',
+      'bibcite_year' => [
+        'value' => 1980,
+      ],
+      'distribution' => [
+          [
+            'value' => 'citation_distribute_repec',
+          ],
+      ],
+    ]);
+
+    $reference->save();
+
+    $this->markEntityForCleanup($reference);
+
+    return $reference;
+  }
+
+}
