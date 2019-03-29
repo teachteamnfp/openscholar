46a40d70fd638e1a71336373f7ebf750a84522f4
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-03-29 14:45:16 +0100

    Issue #11025 - Implement new widget plugin, create new theme for markup

--- profile/modules/apps/os_widgets/src/OsWidgetsBase.php
+++ profile/modules/apps/os_widgets/src/OsWidgetsBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\os_widgets;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
@@ -15,13 +16,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class OsWidgetsBase extends PluginBase implements ContainerFactoryPluginInterface {
 
   protected $entityTypeManager;
+  protected $connection;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct($configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct($configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $connection) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
+    $this->connection = $connection;
   }
 
   /**
@@ -32,7 +35,8 @@ class OsWidgetsBase extends PluginBase implements ContainerFactoryPluginInterfac
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('database')
     );
   }
 
