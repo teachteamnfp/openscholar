b335f2ec30f2e2b9f4c73274b81896f4d12930a1
Matt Petrovic <mpetrovic@guneva.net>
2019-04-03 15:06:11 -0400

    #10894: Code style.

--- profile/modules/vsite/src/Plugin/VsiteTypedDataManager.php
+++ profile/modules/vsite/src/Plugin/VsiteTypedDataManager.php
@@ -2,28 +2,32 @@
 
 namespace Drupal\vsite\Plugin;
 
-
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\DependencyInjection\ClassResolverInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\TypedData\TypedDataManager;
 use Drupal\Core\Validation\ConstraintManager;
-use Drupal\group\Entity\GroupInterface;
 use Drupal\vsite\Event\VsiteActivatedEvent;
 use Drupal\vsite\VsiteEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Class VsiteTypedDataManager.
+ *
  * @package Drupal\vsite\Plugin
  */
 class VsiteTypedDataManager extends TypedDataManager implements EventSubscriberInterface {
 
   /**
-   * @var GroupInterface
+   * Store the active Vsite.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
    */
   protected $activeVsite;
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler, ClassResolverInterface $class_resolver) {
     parent::__construct($namespaces, $cache_backend, $module_handler, $class_resolver);
     $this->setValidationConstraintManager(new ConstraintManager($namespaces, $cache_backend, $module_handler));
@@ -46,37 +50,41 @@ class VsiteTypedDataManager extends TypedDataManager implements EventSubscriberI
   }
 
   /**
-   * Change the cache key
+   * Change the cache key to include the vsite.
    *
-   * @param VsiteActivatedEvent $vsite
+   * @param \Drupal\vsite\Event\VsiteActivatedEvent $vsite
+   *   The activation event.
    */
   public function onVsiteActivated(VsiteActivatedEvent $vsite) {
     if (strpos($this->cacheKey, 'vsite') === FALSE) {
       $this->activeVsite = $vsite->getGroup();
       $this->cacheKey .= '&vsite=' . $this->activeVsite->id();
-      $this->definitions = null;
+      $this->definitions = NULL;
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
   protected function findDefinitions() {
     $definitions = parent::findDefinitions();
 
-    if ($this->activeVsite != null) {
+    if ($this->activeVsite != NULL) {
       $template = [
         'class' => 'Drupal\Core\Entity\Plugin\DataType\EntityAdapter',
         'label' => '',
         'constraints' => [
-          'EntityChanged' => null,
-          'EntityUntranslatableFields' => null
+          'EntityChanged' => NULL,
+          'EntityUntranslatableFields' => NULL,
         ],
         'definition_class' => '\Drupal\Core\Entity\TypedData\EntityDataDefinition',
         'list_class' => '\Drupal\Core\TypedData\Plugin\DataType\ItemList',
         'list_definition_class' => '\Drupal\Core\TypedData\ListDataDefinition',
-        'unwrap_for_canonical_representation' => true,
+        'unwrap_for_canonical_representation' => TRUE,
         'id' => 'entity',
         'description' => t('All kind of entities, e.g. nodes, comments or users.'),
         'deriver' => '\Drupal\Core\Entity\Plugin\DataType\Deriver\EntityDeriver',
-        'provider' => 'core'
+        'provider' => 'core',
       ];
       $configs = \Drupal::configFactory()->listAll('taxonomy.vocabulary');
 
@@ -84,23 +92,16 @@ class VsiteTypedDataManager extends TypedDataManager implements EventSubscriberI
         $definitionName = str_replace('taxonomy.vocabulary.', 'entity:taxonomy_term:', $config_name);
         if (isset($definitions[$definitionName])) {
           unset($definitions[$definitionName]);
-        } else {
+        }
+        else {
           $definitions[$definitionName] = [
-              'label' => $config_name
-            ] + $template;
+            'label' => $config_name,
+          ] + $template;
         }
       }
     }
 
     return $definitions;
-  }
-
-  public function getValidationConstraintManager() {
-    if ($manager = parent::getValidationConstraintManager()) {
-      return $manager;
-    }
-    else {
-    }
   }
 
 }
