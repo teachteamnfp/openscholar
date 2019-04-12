dbd1908c0d4205f1dbd0612ccc00130b895009e1
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-04-12 15:29:40 +0530

    #11369: added commenting and replacing jquery with $

--- profile/modules/custom/os_fullcalendar/js/os_fullcalendar.fullcalendar.js
+++ profile/modules/custom/os_fullcalendar/js/os_fullcalendar.fullcalendar.js
@@ -30,6 +30,7 @@
             tableSubHeaders.each(function () {
               $(this).nextUntil(".fc-list-heading").wrapAll("<tr class='fc-list-item-parent'></tr>");
             });
+            //wrapping events date, moth, year to seperate span for ui
             $('.fc-list-heading-main').each(function () {
               let eventdata = $(this).text().split(' ');
               $(this).empty();
@@ -37,8 +38,9 @@
               $(this).append($("<span class='event-start-month'>").text(eventdata[1]));
               $(this).append($("<span class='event-start-day'>").text(eventdata[2]));
             });
-            var elems = jQuery(".fc-listUpcoming-view tbody > tr, .fc-listPast-view tbody > tr");
-            var wrapper = jQuery('<tr class="fc-wrapper" />');
+            //wrapping every 2 tr(event date and event title, location) to single tr so that UI doesn't break in small screens
+            var elems = $(".fc-listUpcoming-view tbody > tr, .fc-listPast-view tbody > tr");
+            var wrapper = $('<tr class="fc-wrapper" />');
             var pArrLen = elems.length;
             for (var i = 0; i < pArrLen; i += 2) {
               elems.filter(':eq(' + i + '),:eq(' + (i + 1) + ')').wrapAll(wrapper);
