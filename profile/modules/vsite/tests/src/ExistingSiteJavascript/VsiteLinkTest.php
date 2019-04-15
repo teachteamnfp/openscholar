2845ddc680261c8e61c895595d83a2c8ef015c92
Matt Petrovic <mpetrovic@guneva.net>
2019-04-15 10:26:45 -0400

    #11292: Code styl

--- profile/modules/vsite/tests/src/ExistingSiteJavascript/VsiteLinkTest.php
+++ profile/modules/vsite/tests/src/ExistingSiteJavascript/VsiteLinkTest.php
@@ -2,13 +2,9 @@
 
 namespace Drupal\Tests\vsite\ExistingSiteJavascript;
 
-use Drupal\Component\Render\FormattableMarkup;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Url;
-use Drupal\user\Entity\User;
-
 /**
- * Class LinkTest
+ * Class LinkTest.
+ *
  * @package Drupal\Tests\vsite\ExistingSite
  * @group link
  * @group existing-javascript
@@ -16,43 +12,48 @@ use Drupal\user\Entity\User;
 class VsiteLinkTest extends VsiteExistingSiteJavascriptTestBase {
 
   /**
+   * The group we're testing against.
+   *
    * @var \Drupal\group\Entity\GroupInterface
    */
   protected $group;
 
   /**
+   * The generated alias.
+   *
    * @var string
    */
   protected $groupAlias;
 
   /**
-   * @var \Drupal\vsite\Plugin\VsiteContextManagerInterface
+   * {@inheritdoc}
    */
-  protected $vsiteContextManager;
-
   public function setUp() {
     parent::setUp();
     $this->groupAlias = $this->randomMachineName();
     $this->group = $this->createGroup([
       'path' => [
-        'alias' => '/'.$this->groupAlias
-      ]
+        'alias' => '/' . $this->groupAlias,
+      ],
     ]);
-
-    $this->vsiteContextManager = $this->container->get('vsite.context_manager');
   }
 
   /**
+   * Test that our controller outputs the right strings.
+   *
+   * These strings depict underlying request state.
+   *
    * @throws \Behat\Mink\Exception\ResponseTextException
    */
   public function testLinks() {
-    $this->visit('/'.$this->groupAlias);
+    $this->visit('/' . $this->groupAlias);
     $this->assertSession()->statusCodeNotEquals(404);
-    $this->visit('/'.$this->groupAlias.'/link-test');
+    $this->visit('/' . $this->groupAlias . '/link-test');
     $this->assertSession()->statusCodeEquals(200);
     $page = $this->getCurrentPage();
     $this->assertSession()->pageTextContains('vsite active');
 
-    $this->assertSession()->pageTextContains($this->groupAlias.'/link-test');
+    $this->assertSession()->pageTextContains($this->groupAlias . '/link-test');
   }
+
 }
