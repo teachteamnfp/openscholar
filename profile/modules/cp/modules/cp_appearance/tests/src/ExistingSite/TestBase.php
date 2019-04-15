a3f1111e4635888accf63c8e3b42c540352fb839
Subhojit Paul <subhojitpaul21@gmail.com>
2019-04-15 12:39:49 +0530

    Issue #11364: Better test management
    Multiple tests require admin user, therefore it would be created in the
    setup.

--- profile/modules/cp/modules/cp_appearance/tests/src/ExistingSite/TestBase.php
+++ profile/modules/cp/modules/cp_appearance/tests/src/ExistingSite/TestBase.php
@@ -39,14 +39,34 @@ abstract class TestBase extends ExistingSiteBase {
   protected $appearanceHelper;
 
   /**
+   * Default theme.
+   *
+   * @var string
+   */
+  protected $defaultTheme;
+
+  /**
+   * Administrator.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $admin;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
+
+    $this->admin = $this->createUser([], NULL, TRUE);
+
     $this->entityTypeManager = $this->container->get('entity_type.manager');
     $this->vsiteContextManager = $this->container->get('vsite.context_manager');
     $this->configFactory = $this->container->get('config.factory');
     $this->appearanceHelper = $this->container->get('cp_appearance.appearance_helper');
+    /** @var \Drupal\Core\Config\ImmutableConfig $theme_config */
+    $theme_config = $this->configFactory->get('system.theme');
+    $this->defaultTheme = $theme_config->get('default');
   }
 
   /**
@@ -73,6 +93,16 @@ abstract class TestBase extends ExistingSiteBase {
     $this->markEntityForCleanup($group);
 
     return $group;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    parent::tearDown();
+    /** @var \Drupal\Core\Config\Config $theme_config_mut */
+    $theme_config_mut = $this->configFactory->getEditable('system.theme');
+    $theme_config_mut->set('default', $this->defaultTheme)->save();
   }
 
 }
