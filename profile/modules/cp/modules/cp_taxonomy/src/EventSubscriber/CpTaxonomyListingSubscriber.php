22cd48291a846ca39d538d9405c13e2385d73801
Matt Petrovic <mpetrovic@guneva.net>
2019-04-16 13:41:06 -0400

    profile/modules/cp/modules/cp_taxonomy/

---
+++ profile/modules/cp/modules/cp_taxonomy/src/EventSubscriber/CpTaxonomyListingSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\cp_taxonomy\EventSubscriber;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class CpTaxonomyListingSubscriber.
+ *
+ * @package Drupal\cp_taxonomy\EventSubscriber
+ */
+class CpTaxonomyListingSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = [];
+    $events[KernelEvents::VIEW][] = ['onView', 1000];
+    return $events;
+  }
+
+  /**
+   * Change out the Add Taxonomy link for our own.
+   *
+   * @param GetResponseForControllerResultEvent $e
+   *    The event for the response.
+   */
+  public function onView(GetResponseForControllerResultEvent $e) {
+    if ($e->getRequest()->attributes->get('_route') == 'cp.taxonomy') {
+      $build_array = $e->getControllerResult();
+
+      /** @var TranslatableMarkup $emptyMarkup */
+      $emptyMarkup = $build_array['table']['#empty'];
+      $url = Url::fromRoute('cp.taxonomy.add');
+      $build_array['table']['#empty'] = t($emptyMarkup->getUntranslatedString(), [':link' => $url->toString()]);
+      $e->setControllerResult($build_array);
+    }
+  }
+
+}
