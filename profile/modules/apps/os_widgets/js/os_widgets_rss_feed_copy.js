a49da0aa31ca13338bdf38c1fd07b7909647e0e1
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-04-18 11:46:30 +0200

    Issue #11022 - Put a comment to explain functionality

--- profile/modules/apps/os_widgets/js/os_widgets_rss_feed_copy.js
+++ profile/modules/apps/os_widgets/js/os_widgets_rss_feed_copy.js
@@ -1,9 +1,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.osBoxesRss = {
     attach: function (context, settings) {
+      // Find available RssWidget markup and copy generated link to clipboard.
       $('.rss-feed-link', context).click(function (e) {
         e.preventDefault();
         var rss = document.createElement("input");
+        // Get copiable link from href value.
         rss.setAttribute("value", $(this).attr('href'));
         document.body.appendChild(rss);
         rss.select();
