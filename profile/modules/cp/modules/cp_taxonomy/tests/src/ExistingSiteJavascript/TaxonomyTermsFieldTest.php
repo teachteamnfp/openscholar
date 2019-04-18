ea8434e9e045bcf060efd71916a9b002c68e919c
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-04-18 11:15:25 +0200

    Issue #11352 - Fix extends and remove createGroup function use OsExistingSiteJavascriptTestBase

--- profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/TaxonomyTermsFieldTest.php
+++ profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/TaxonomyTermsFieldTest.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\Tests\cp_taxonomy\ExistingSiteJavascript;
 
+use Drupal\group\Entity\GroupInterface;
+use Drupal\media\MediaInterface;
+use Drupal\node\NodeInterface;
 use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTestBase;
 
 /**
  * Tests taxonomy_terms fields functionality.
@@ -10,7 +14,36 @@ use Drupal\taxonomy\Entity\Vocabulary;
  * @group other
  * @group functional-javascript
  */
-class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
+class TaxonomyTermsFieldTest extends OsExistingSiteJavascriptTestBase {
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Test group 1.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
+   */
+  protected $group1;
+
+  /**
+   * Test group 2.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
+   */
+  protected $group2;
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  protected $vsiteContextManager;
 
   protected $groupUser;
 
@@ -19,6 +52,22 @@ class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
    */
   public function setUp() {
     parent::setUp();
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->config = $this->container->get('config.factory');
+    /** @var \Drupal\vsite\Plugin\VsiteContextManagerInterface $vsite_context_manager */
+    $this->vsiteContextManager = $this->container->get('vsite.context_manager');
+
+    $this->group1 = $this->createGroup([
+      'path' => [
+        'alias' => '/' . $this->randomMachineName(),
+      ],
+    ]);
+    $this->group2 = $this->createGroup([
+      'path' => [
+        'alias' => '/' . $this->randomMachineName(),
+      ],
+    ]);
+
     $this->groupUser = $this->createUser([], NULL, TRUE);
     $this->group1->addMember($this->groupUser);
     $this->group2->addMember($this->groupUser);
@@ -91,6 +140,119 @@ class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
     $page = $this->getCurrentPage();
     $is_exists = $page->hasContent('Tag with Terms');
     $this->assertFalse($is_exists, 'Tag with Terms field is visible.');
+  }
+
+  /**
+   * Creates a taxonomy_test_1.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_1.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created node entity.
+   */
+  protected function createTaxonomyTest1(array $values = []) : NodeInterface {
+    $event = $this->createNode($values + [
+      'type' => 'taxonomy_test_1',
+      'title' => $this->randomString(),
+    ]);
+
+    return $event;
+  }
+
+  /**
+   * Creates a taxonomy_test_2.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_2.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created node entity.
+   */
+  protected function createTaxonomyTest2(array $values = []) : NodeInterface {
+    $event = $this->createNode($values + [
+      'type' => 'taxonomy_test_2',
+      'title' => $this->randomString(),
+    ]);
+
+    return $event;
+  }
+
+  /**
+   * Creates a taxonomy_test_file Media.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_file.
+   *
+   * @return \Drupal\media\MediaInterface
+   *   The created media entity.
+   */
+  protected function createTaxonomyTestFile(array $values = []) : MediaInterface {
+    $media = $this->entityTypeManager->getStorage('media')->create($values + [
+      'type' => 'taxonomy_test_file',
+      'name' => $this->randomMachineName(),
+    ]);
+    $media->enforceIsNew();
+    $media->save();
+
+    $this->markEntityForCleanup($media);
+
+    return $media;
+  }
+
+  /**
+   * Create a vocabulary to a group on cp taxonomy pages.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   Group entity.
+   * @param string $vid
+   *   Vocabulary id.
+   * @param array $allowed_types
+   *   Allowed types for entity bundles.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  protected function createGroupVocabulary(GroupInterface $group, string $vid, array $allowed_types = []) {
+    $this->vsiteContextManager->activateVsite($group);
+    $this->visit($group->get('path')->getValue()[0]['alias'] . '/cp/taxonomy/add');
+    $web_assert = $this->assertSession();
+    $web_assert->statusCodeEquals(200);
+    $page = $this->getCurrentPage();
+    $page->fillField('name', $vid);
+    $page->fillField('vid', $vid);
+    foreach ($allowed_types as $allowed_type) {
+      $page->fillField('allowed_entity_types[' . $allowed_type . ']', $allowed_type);
+    }
+    $submit_button = $page->findButton('Save');
+    $submit_button->press();
+    $web_assert->pageTextContains('Created new vocabulary');
+    $web_assert->statusCodeEquals(200);
+  }
+
+  /**
+   * Create a vocabulary to a group on cp taxonomy pages.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   Group entity.
+   * @param string $vid
+   *   Vocabulary id.
+   * @param string $name
+   *   Taxonomy term name.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  protected function createGroupTerm(GroupInterface $group, string $vid, string $name) {
+    $this->vsiteContextManager->activateVsite($group);
+    $this->visit($group->get('path')->getValue()[0]['alias'] . '/cp/taxonomy/' . $vid . '/add');
+    $web_assert = $this->assertSession();
+    $web_assert->statusCodeEquals(200);
+    $page = $this->getCurrentPage();
+    $page->fillField('name[0][value]', $name);
+    $submit_button = $page->findButton('Save');
+    $submit_button->press();
+    $web_assert->statusCodeEquals(200);
   }
 
   /**
