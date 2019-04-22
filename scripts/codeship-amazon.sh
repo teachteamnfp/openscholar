08fc2130401e608e0c61735abc30f662b35f67d9
Matt Petrovic <mpetrovic@guneva.net>
2019-04-22 17:23:05 -0400

    Remove this

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -88,7 +88,7 @@ if [[ $FORCE_REBUILD == "1" ]] || [[ "$(cmp -b 'openscholar/composer.json' '/tmp
   cd openscholar
 
   # Download composer components
-  yes | composer install --ignore-platform-reqs -y || exit 1
+  yes | composer install --ignore-platform-reqs || exit 1
 
   # Do not use the node_modules symlink, and reinstall node modules
   rm -rf node_modules
