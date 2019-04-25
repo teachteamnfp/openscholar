f40924f869fe5608f0ba2e64dc46c39f62687bd1
Sztreha Balázs <balazs.sztreha@cheppers.com>
2019-04-25 17:35:33 +0200

    Issue #11015 - Start to implement bundle filtering

--- profile/modules/apps/os_widgets/src/Plugin/OsWidgets/TaxonomyWidget.php
+++ profile/modules/apps/os_widgets/src/Plugin/OsWidgets/TaxonomyWidget.php
@@ -4,6 +4,7 @@ namespace Drupal\os_widgets\Plugin\OsWidgets;
 
 use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\Database;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\os_widgets\OsWidgetsBase;
 use Drupal\os_widgets\OsWidgetsInterface;
@@ -51,9 +52,6 @@ class TaxonomyWidget extends OsWidgetsBase implements OsWidgetsInterface {
     $field_taxonomy_vocabulary_values = $block_content->get('field_taxonomy_vocabulary')->getValue();
     $field_taxonomy_tree_depth_values = $block_content->get('field_taxonomy_tree_depth')->getValue();
     $field_taxonomy_show_children_values = $block_content->get('field_taxonomy_show_children')->getValue();
-    $field_taxonomy_range_values = $block_content->get('field_taxonomy_range')->getValue();
-    $field_taxonomy_offset_values = $block_content->get('field_taxonomy_offset')->getValue();
-    $field_taxonomy_show_term_desc_values = $block_content->get('field_taxonomy_show_term_desc')->getValue();
     $field_taxonomy_display_type_values = $block_content->get('field_taxonomy_display_type')->getValue();
     $vid = $field_taxonomy_vocabulary_values[0]['target_id'];
     $depth = empty($field_taxonomy_tree_depth_values[0]['value']) ? NULL : $field_taxonomy_tree_depth_values[0]['value'];
@@ -65,6 +63,126 @@ class TaxonomyWidget extends OsWidgetsBase implements OsWidgetsInterface {
     $settings['depth'] = $depth;
     $settings['bundles'] = $this->getFilteredBundles($block_content);
     $terms = $this->getTerms($settings);
+    $term_items = $this->getRenderableTerms($block_content, $terms);
+    switch ($field_taxonomy_display_type_values[0]['value']) {
+      case 'menu':
+        $theme = 'os_widgets_taxonomy_display_type_menu';
+        break;
+
+      case 'slider':
+        $theme = 'os_widgets_taxonomy_display_type_slider';
+        break;
+
+      default:
+        $theme = 'item_list';
+        break;
+    }
+    $build['taxonomy']['terms'] = [
+      '#theme' => $theme,
+      '#items' => $term_items,
+    ];
+  }
+
+  /**
+   * Collect terms by field values.
+   *
+   * @param array $settings
+   *   Parameters to select terms.
+   *
+   * @return array
+   *   List of filtered terms.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  protected function getTerms(array $settings) {
+    $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($settings['vid'], 0, $settings['depth']);
+    if (empty($terms) || empty($settings['bundles'])) {
+      return $terms;
+    }
+    $tids = [];
+    foreach ($terms as $term) {
+      $tids[] = $term->tid;
+    }
+    $query = Database::getConnection()->select('taxonomy_term_data', 'td');
+    $query->fields('td', ['tid']);
+    $query->condition('td.tid', $tids, 'IN');
+    $entities = $this->explodeEntityBundles($settings['bundles']);
+    foreach ($entities as $entity_name => $bundles) {
+      $alias = $entity_name . '_ftt';
+      $query->leftJoin($entity_name . '__field_taxonomy_terms', $alias, $alias . '.field_taxonomy_terms_target_id = td.tid');
+      $query->condition($alias . '.bundle', $bundles, 'IN');
+      $query->fields($alias, ['entity_id']);
+    }
+
+    $result = $query->execute();
+    $filtered_tids = [];
+    while ($row = $result->fetchAssoc()) {
+      $filtered_tids[] = $row['tid'];
+    }
+    foreach ($terms as $i => $term) {
+      if (!in_array($term->tid, $filtered_tids)) {
+        unset($terms[$i]);
+      }
+    }
+    return $terms;
+  }
+
+  /**
+   * Explode entity bundles.
+   */
+  private function explodeEntityBundles($bundles) {
+    $entities = [];
+    foreach ($bundles as $bundle) {
+      list($entity_name, $bundle) = explode(':', $bundle);
+      $entities[$entity_name][] = $bundle;
+    }
+    return $entities;
+  }
+
+  /**
+   * Collect bundles in array depend on other field selection.
+   *
+   * @param \Drupal\block_content\Entity\BlockContent $block_content
+   *   Block Content entity.
+   *
+   * @return array
+   *   Collected bundles array.
+   */
+  protected function getFilteredBundles(BlockContent $block_content): array {
+    $field_taxonomy_behavior_values = $block_content->get('field_taxonomy_behavior')->getValue();
+    $bundles = [];
+    switch ($field_taxonomy_behavior_values[0]['value']) {
+      case 'select':
+        if (!empty($block_content->get('field_taxonomy_bundle')->value)) {
+          $bundles[] = $block_content->get('field_taxonomy_bundle')->value;
+        }
+        break;
+
+      case 'contextual':
+        if ($node = $this->requestStack->getCurrentRequest()->attributes->get('node')) {
+          $bundles[] = 'node:' . $node->bundle();
+        }
+        break;
+    }
+    return $bundles;
+  }
+
+  /**
+   * Create a renderable array from terms entities.
+   *
+   * @param \Drupal\block_content\Entity\BlockContent $block_content
+   *   Block content entity.
+   * @param array $terms
+   *   Filtered terms array.
+   *
+   * @return array
+   *   Renderable list array.
+   */
+  protected function getRenderableTerms(BlockContent $block_content, array $terms): array {
+    $field_taxonomy_show_term_desc_values = $block_content->get('field_taxonomy_show_term_desc')->getValue();
+    $field_taxonomy_range_values = $block_content->get('field_taxonomy_range')->getValue();
+    $field_taxonomy_offset_values = $block_content->get('field_taxonomy_offset')->getValue();
     $term_items = [];
     $count = 0;
     $offset = 0;
@@ -106,67 +224,7 @@ class TaxonomyWidget extends OsWidgetsBase implements OsWidgetsInterface {
         '#description' => $description,
       ];
     }
-    switch ($field_taxonomy_display_type_values[0]['value']) {
-      case 'menu':
-        $theme = 'os_widgets_taxonomy_display_type_menu';
-        break;
-
-      case 'slider':
-        $theme = 'os_widgets_taxonomy_display_type_slider';
-        break;
-
-      default:
-        $theme = 'item_list';
-        break;
-    }
-    $build['taxonomy']['terms'] = [
-      '#theme' => $theme,
-      '#items' => $term_items,
-    ];
-  }
-
-  /**
-   * Collect terms by field values.
-   *
-   * @param array $settings
-   *   Parameters to select terms.
-   *
-   * @return array
-   *   List of filtered terms.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   */
-  protected function getTerms(array $settings) {
-    $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($settings['vid'], 0, $settings['depth']);
-    return $terms;
-  }
-
-  /**
-   * Collect bundles in array depend on other field selection.
-   *
-   * @param \Drupal\block_content\Entity\BlockContent $block_content
-   *   Block Content entity.
-   *
-   * @return array
-   *   Collected bundles array.
-   */
-  protected function getFilteredBundles(BlockContent $block_content): array {
-    $field_taxonomy_behavior_values = $block_content->get('field_taxonomy_behavior')->getValue();
-    $bundles = [];
-    if ($field_taxonomy_behavior_values[0]['value'] == 'select') {
-      $field_taxonomy_bundle_values = $block_content->get('field_taxonomy_bundle')
-        ->getValue();
-      foreach ($field_taxonomy_bundle_values as $field_taxonomy_bundle_value) {
-        $bundles[] = $field_taxonomy_bundle_value['value'];
-      }
-    }
-    if ($field_taxonomy_behavior_values[0]['value'] == 'contextual') {
-      if ($node = $this->requestStack->getCurrentRequest()->attributes->get('node')) {
-        $bundles[] = $node->bundle();
-      }
-    }
-    return $bundles;
+    return $term_items;
   }
 
 }
