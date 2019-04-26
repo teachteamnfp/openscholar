fe14756750409631b5566f7c6b2ee88633ec9aa5
Matt Petrovic <mpetrovic@guneva.net>
2019-04-26 14:07:33 -0400

    ugh

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -52,7 +52,7 @@ cp -rf . /tmp/
 
 cd $BUILD_ROOT
 
-echo $(git log --grep="^git-subtree-dir: openscholar/*\$")
+echo $(git log --grep="^git-subtree-dir: openscholar/*\$" --no-show-signature --pretty-format:'START %H%n%s%n%n%b%nEND%n')
 
 git subtree pull -d -m "$CI_MESSAGE" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH --squash
 
