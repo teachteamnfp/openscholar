d43499362c86408ac5ac288b670ceb8c89cbc0d6
Subhojit Paul <subhojitpaul21@gmail.com>
2019-04-29 16:09:30 +0530

    Issue #11293: AddClassMaterialLinkTest using group permissions

--- profile/modules/apps/os_classes/tests/src/ExistingSite/AddClassMaterialLinkTest.php
+++ profile/modules/apps/os_classes/tests/src/ExistingSite/AddClassMaterialLinkTest.php
@@ -14,13 +14,6 @@ use Drupal\Tests\openscholar\ExistingSite\OsExistingSiteTestBase;
 class AddClassMaterialLinkTest extends OsExistingSiteTestBase {
 
   /**
-   * Test group.
-   *
-   * @var \Drupal\group\Entity\GroupInterface
-   */
-  protected $group;
-
-  /**
    * Administrator and group administrator.
    *
    * @var \Drupal\user\UserInterface
@@ -47,7 +40,7 @@ class AddClassMaterialLinkTest extends OsExistingSiteTestBase {
   public function setUp() {
     parent::setUp();
 
-    $this->adminUser = $this->createUser([], '', TRUE);
+    $this->adminUser = $this->createUser();
     $this->simpleUser = $this->createUser();
 
     $this->class = $this->createNode([
@@ -57,27 +50,20 @@ class AddClassMaterialLinkTest extends OsExistingSiteTestBase {
       'field_class_materials[0][subform][field_title][0][value]' => $this->randomString(),
       'field_class_materials[0][subform][field_body][0][value]' => $this->randomString(),
     ]);
-    $this->group = $this->createGroup();
-    $this->group->addMember($this->adminUser, [
-      'group_roles' => [
-        'personal-administrator',
-      ],
-    ]);
+    $this->addGroupAdmin($this->adminUser, $this->group);
     $this->group->addContent($this->class, 'group_node:class');
   }
 
   /**
    * Test Add Class material link as admin.
    *
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    * @throws \Behat\Mink\Exception\ExpectationException
    */
   public function testAddMaterialLinkAsAdmin(): void {
     $this->drupalLogin($this->adminUser);
-    /** @var \Drupal\Core\Path\AliasManagerInterface $path_alias_manager */
-    $path_alias_manager = $this->container->get('path.alias_manager');
-    $group_alias = $path_alias_manager->getAliasByPath("/group/{$this->group->id()}");
 
-    $this->drupalGet("$group_alias/node/{$this->class->id()}");
+    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/node/{$this->class->id()}");
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->linkByHrefExists('add/paragraph/class_material');
   }
@@ -86,14 +72,12 @@ class AddClassMaterialLinkTest extends OsExistingSiteTestBase {
    * Test Add class material link as a non-member.
    *
    * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    */
   public function testAddMaterialLinkAsOutsider(): void {
     $this->drupalLogin($this->simpleUser);
-    /** @var \Drupal\Core\Path\AliasManagerInterface $path_alias_manager */
-    $path_alias_manager = $this->container->get('path.alias_manager');
-    $group_alias = $path_alias_manager->getAliasByPath("/group/{$this->group->id()}");
 
-    $this->drupalGet("$group_alias/node/{$this->class->id()}");
+    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/node/{$this->class->id()}");
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->linkByHrefNotExists('add/paragraph/class_material');
   }
@@ -102,33 +86,28 @@ class AddClassMaterialLinkTest extends OsExistingSiteTestBase {
    * Test Add Class material Link on classes view as Admin user.
    *
    * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    */
   public function testAddLinkOnClassesViewAsAdmin(): void {
     $this->drupalLogin($this->adminUser);
-    /** @var \Drupal\Core\Path\AliasManagerInterface $path_alias_manager */
-    $path_alias_manager = $this->container->get('path.alias_manager');
-    $group_alias = $path_alias_manager->getAliasByPath("/group/{$this->group->id()}");
 
-    $this->visit("$group_alias/classes");
+    $this->visit("{$this->group->get('path')->first()->getValue()['alias']}/classes");
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkByHrefExists("$group_alias/node/{$this->class->id()}/add/paragraph/class_material");
+    $this->assertSession()->linkByHrefExists("{$this->group->get('path')->first()->getValue()['alias']}/node/{$this->class->id()}/add/paragraph/class_material");
   }
 
   /**
    * Test Add Class material Link on classes view as a non-member.
    *
    * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
    */
   public function testAddLinkOnClassesViewAsOutsider(): void {
     $this->drupalLogin($this->simpleUser);
 
-    /** @var \Drupal\Core\Path\AliasManagerInterface $path_alias_manager */
-    $path_alias_manager = $this->container->get('path.alias_manager');
-    $group_alias = $path_alias_manager->getAliasByPath("/group/{$this->group->id()}");
-
-    $this->visit("$group_alias/classes");
+    $this->visit("{$this->group->get('path')->first()->getValue()['alias']}/classes");
     $this->assertSession()->statusCodeEquals(403);
-    $this->assertSession()->linkByHrefNotExists("$group_alias/node/{$this->class->id()}/add/paragraph/class_material");
+    $this->assertSession()->linkByHrefNotExists("{$this->group->get('path')->first()->getValue()['alias']}/node/{$this->class->id()}/add/paragraph/class_material");
   }
 
 }
