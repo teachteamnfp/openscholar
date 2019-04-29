6d2708de27b0e5a2df7f6bc2fd9fdbd453f219f2
Subhojit Paul <subhojitpaul21@gmail.com>
2019-04-29 15:26:50 +0530

    Issue #11293: FlavorFormTest using group permissions

--- profile/modules/cp/modules/cp_appearance/tests/src/ExistingSiteJavascript/FlavorFormTest.php
+++ profile/modules/cp/modules/cp_appearance/tests/src/ExistingSiteJavascript/FlavorFormTest.php
@@ -19,6 +19,11 @@ class FlavorFormTest extends OsExistingSiteJavascriptTestBase {
    */
   protected $group;
 
+  /**
+   * Default theme name.
+   *
+   * @var string
+   */
   protected $defaultTheme;
 
   /**
@@ -36,9 +41,9 @@ class FlavorFormTest extends OsExistingSiteJavascriptTestBase {
         'alias' => '/cp-appearance-flavor',
       ],
     ]);
-    $admin = $this->createAdminUser();
+    $admin = $this->createUser();
 
-    $this->group->addMember($admin);
+    $this->addGroupAdmin($admin, $this->group);
 
     $this->drupalLogin($admin);
   }
