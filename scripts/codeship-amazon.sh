e511ed6cfb7e1eb7ec5b0472430a06774843789e
Richard Brandon <Richard.Brandon@gmail.com>
2019-04-30 15:05:06 -0400

    restore squash

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -52,7 +52,7 @@ cp -rf . /tmp/
 
 cd $BUILD_ROOT
 
-git subtree pull -q -m "$CI_MESSAGE" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH 
+git subtree pull -q -m "$CI_MESSAGE" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH --squash
 
 cd openscholar/profile/themes
 
