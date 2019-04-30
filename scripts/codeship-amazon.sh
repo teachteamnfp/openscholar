66c8401850abd32b6e6f5c928afb690a44024d29
Richard Brandon <Richard.Brandon@gmail.com>
2019-04-30 14:57:30 -0400

    CI MESSAGE change.

--- scripts/codeship-amazon.sh
+++ scripts/codeship-amazon.sh
@@ -51,8 +51,8 @@ cd openscholar/profile/themes
 cp -rf . /tmp/
 
 cd $BUILD_ROOT
-
-git subtree pull -q -m "$CI_MESSAGE" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH --squash
+echo "$CI_MESSAGE";
+git subtree pull -q -m "merge commit" --prefix=openscholar git://github.com/openscholar/openscholar.git $CI_BRANCH --squash
 
 cd openscholar/profile/themes
 
