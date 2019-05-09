a5797e6962ff11a3ffcf55d257d3119cf0b2d9fe
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-05-09 20:50:47 +0200

    Issue #11297 - Ckeditor OS link plugin basics (plugin js sample)

---
+++ profile/modules/os/modules/os_wysiwyg/src/Plugin/CKEditorPlugin/OsLink.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\os_wysiwyg\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "os_link" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "os_link",
+ *   label = @Translation("Link")
+ * )
+ */
+class OsLink extends CKEditorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    return drupal_get_path('module', 'os_wysiwyg') . '/js/plugins/os_link/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibraries(Editor $editor) {
+    return [
+      'core/drupal.ajax',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return [
+      'osLink_dialogLinkAdd' => t('Add link'),
+      'osLink_dialogLinkEdit' => t('Edit link'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getButtons() {
+    $path = drupal_get_path('module', 'os_wysiwyg') . '/js/plugins/os_link';
+    return [
+      'OsLink' => [
+        'label' => $this->t('Link'),
+        'image' => $path . '/icons/oslink.png',
+      ],
+      'OsUnLink' => [
+        'label' => $this->t('Unlink'),
+        'image' => $path . '/icons/osunlink.png',
+      ],
+    ];
+  }
+
+}
