40acac5144b4721a9a446df793ad7bebb44638ba
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2019-05-09 18:33:32 +0530

    Form and hook to alter js.

--- profile/modules/os/modules/os_google_analytics/tests/src/ExistingSite/GaFormTest.php
+++ profile/modules/os/modules/os_google_analytics/tests/src/ExistingSite/GaFormTest.php
@@ -22,6 +22,8 @@ class GaFormTest extends ExistingSiteBase {
 
     $this->adminUser = $this->createUser([], '', TRUE);
     $this->simpleUser = $this->createUser();
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->vsiteContextManager = $this->container->get('vsite.context_manager');
   }
 
   /**
@@ -50,10 +52,59 @@ class GaFormTest extends ExistingSiteBase {
     $this->drupalGet('cp/settings/analytics');
     // Dummy web property.
     $edit = [
-      'edit-web-property-id' => 'UA-1234567-A1',
+      'edit-web-property-id' => 'UA-111111111-1',
     ];
     $this->submitForm($edit, 'edit-submit');
     $this->assertSession()->fieldValueEquals('edit-web-property-id', 'UA-1234567-A1');
+  }
+
+  /**
+   * Test Settings form.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  public function testVsiteCodesShowOnPage() {
+
+    $vsite = $this->createGroup([
+      'type' => 'personal',
+      'path' => [
+        'alias' => '/test-alias',
+      ],
+    ]);
+    $this->vsiteContextManager->activateVsite($vsite);
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('test-alias/cp/settings/analytics');
+
+    // Dummy web property.
+    $edit = [
+      'edit-web-property-id' => 'UA-111111111-1',
+    ];
+    $this->submitForm($edit, 'edit-submit');
+    $this->drupalGet('test-alias');
+    $this->assertSession()->responseContains('ga("test-alias.send", "pageview")');
+    $this->assertSession()->responseContains('UA-111111111-1');
+  }
+
+  /**
+   * Create a vsite.
+   *
+   * @param array $values
+   *   The values for the new vsite.
+   *
+   * @return \Drupal\group\Entity\GroupInterface
+   *   The created group entity.
+   */
+  protected function createGroup(array $values = []) {
+    $group = $this->entityTypeManager->getStorage('group')->create($values + [
+      'type' => 'personal',
+      'label' => $this->randomMachineName(),
+    ]);
+    $group->enforceIsNew();
+    $group->save();
+
+    $this->markEntityForCleanup($group);
+
+    return $group;
   }
 
 }
