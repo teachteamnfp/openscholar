c5d116bffdd42b42a6cdd642d98ebea28d314491
Matt Petrovic <mpetrovic@guneva.net>
2019-05-17 10:22:12 -0400

    #11027: Get ready to activate the context

--- profile/modules/apps/os_widgets/src/EventSubscriber/LayoutContextActivator.php
+++ profile/modules/apps/os_widgets/src/EventSubscriber/LayoutContextActivator.php
@@ -3,6 +3,7 @@
 namespace Drupal\os_widgets\EventSubscriber;
 
 
+use Drupal\Core\Routing\RouteMatch;
 use Drupal\os_widgets\LayoutContextInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
@@ -25,10 +26,23 @@ class LayoutContextActivator implements EventSubscriberInterface {
     /** @var LayoutContextInterface[] $contexts */
     $contexts = \Drupal::entityTypeManager()->getStorage('layout_context')->loadMultiple();
     $request = $event->getRequest();
+    $routeMatch = RouteMatch::createFromRequest($request);
+    $route = $routeMatch->getRouteObject();
 
     foreach ($contexts as $layoutContext) {
       $rules = $layoutContext->getActivationRules();
+      $lines = preg_split("|[\n\r]", $rules);
+      foreach ($lines as $l) {
+        $regex = $this->convertRuleToRegex($l);
+        if (preg_match($regex, $route->getPath()) || preg_match($regex, $routeMatch->getRouteName())) {
+          // what do I do here
+        }
+      }
     }
+  }
+
+  protected function convertRuleToRegex($rule) {
+    return '|^'.str_replace('*', '.*', $rule).'$|';
   }
 
 }
