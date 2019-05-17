d8fbb105263e96a705176fc96b8a7e14f5cdf974
Subhojit Paul <subhojitpaul21@gmail.com>
2019-05-17 10:14:29 +0530

    Issue #11605: testAddEvent

--- profile/modules/apps/os_events/tests/src/ExistingSite/ViewsIcalTest.php
+++ profile/modules/apps/os_events/tests/src/ExistingSite/ViewsIcalTest.php
@@ -142,4 +142,84 @@ class ViewsIcalTest extends EventsTestBase {
     }
   }
 
+  /**
+   * @covers ::addEvent
+   * @throws \Exception
+   */
+  public function testAddEvent(): void {
+    /** @var \Drupal\views_ical\ViewsIcalHelperInterface $views_ical_helper */
+    $views_ical_helper = $this->container->get('views_ical.helper');
+    $field_mapping = [
+      'date_field' => 'field_open_date',
+      'summary_field' => 'title',
+      'location_field' => 'field_location',
+      'description_field' => 'body',
+    ];
+
+    // Test without end date.
+    $event = $this->createEvent([
+      'title' => 'Test Event',
+      'field_open_date' => [
+        'value' => '2019-01-29T10:00:00',
+      ],
+      'status' => TRUE,
+      'field_location' => [
+        'value' => 'Pluto',
+      ],
+      'body' => [
+        'value' => 'Inter-Galactic Event in a non-planet',
+      ],
+    ]);
+
+    $events = [];
+    $views_ical_helper->addEvent($events, $event, new \DateTimeZone($this->user->getTimeZone()), $field_mapping);
+
+    $this->assertCount(1, $events);
+
+    foreach ($events as $item) {
+      $ical = $item->render();
+
+      $this->assertNotNull($item->getDtStart());
+      $this->assertNull($item->getDtEnd());
+      $this->assertTrue($item->getUseTimezone());
+
+      $this->assertContains('SUMMARY:Test Event', $ical);
+      $this->assertContains('LOCATION:Pluto', $ical);
+      $this->assertContains('DESCRIPTION:Inter-Galactic Event in a non-planet', $ical);
+    }
+
+    // Test with end date.
+    $event = $this->createEvent([
+      'title' => 'Test Event',
+      'field_open_date' => [
+        'value' => '2019-01-29T10:00:00',
+        'end_value' => '2019-01-29T11:00:00',
+      ],
+      'status' => TRUE,
+      'field_location' => [
+        'value' => 'Pluto',
+      ],
+      'body' => [
+        'value' => 'Inter-Galactic Event in a non-planet',
+      ],
+    ]);
+
+    $events = [];
+    $views_ical_helper->addEvent($events, $event, new \DateTimeZone($this->user->getTimeZone()), $field_mapping);
+
+    $this->assertCount(1, $events);
+
+    foreach ($events as $item) {
+      $ical = $item->render();
+
+      $this->assertNotNull($item->getDtStart());
+      $this->assertNotNull($item->getDtEnd());
+      $this->assertTrue($item->getUseTimezone());
+
+      $this->assertContains('SUMMARY:Test Event', $ical);
+      $this->assertContains('LOCATION:Pluto', $ical);
+      $this->assertContains('DESCRIPTION:Inter-Galactic Event in a non-planet', $ical);
+    }
+  }
+
 }
