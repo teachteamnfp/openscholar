bfc4b6dd04199d1be32013c1b98bc5ad9df4c20e
Subhojit Paul <subhojitpaul21@gmail.com>
2019-05-17 12:37:50 +0530

    Issue #11605: Code refactoring
    Test content creation methods in place. This is for functional JS tests.

--- profile/modules/apps/os_events/tests/src/Traits/EventTestTrait.php
+++ profile/modules/apps/os_events/tests/src/Traits/EventTestTrait.php
@@ -49,4 +49,52 @@ trait EventTestTrait {
     $this->submitForm($edit, 'edit-submit');
   }
 
+  /**
+   * Creates an Event entity.
+   *
+   * @return string
+   *   The url to newly created entity.
+   */
+  public function createEventFunctionalJs(): string {
+    $date = new DateTimePlus('+5 days');
+
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('node/add/events');
+    $edit = [
+      'title[0][value]' => $this->randomString(),
+      'field_recurring_date[0][day_start]' => $date->format("Y-m-d"),
+      'field_recurring_date[0][day_end]' => $date->format("Y-m-d"),
+      'field_recurring_date[0][is_all_day]' => TRUE,
+      'field_signup[value]' => TRUE,
+    ];
+    $this->submitForm($edit, 'edit-submit');
+    return $this->getUrl();
+  }
+
+  /**
+   * Creates an Event entity.
+   *
+   * @return string
+   *   The url to newly created entity.
+   */
+  public function createRecurringEvent(): string {
+    $date = new DateTimePlus('+5 day');
+
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('node/add/events');
+    $edit = [
+      'title[0][value]' => $this->randomString(),
+      'field_recurring_date[0][day_start]' => $date->format("Y-m-d"),
+      'field_recurring_date[0][day_end]' => $date->format("Y-m-d"),
+      'field_recurring_date[0][is_all_day]' => TRUE,
+      'field_recurring_date[0][recurrence_option]' => 'daily',
+      'field_recurring_date[0][ends_mode]' => TRUE,
+      'field_recurring_date[0][ends_count]' => 3,
+      'field_signup[value]' => TRUE,
+      'field_singup_multiple[value]' => TRUE,
+    ];
+    $this->submitForm($edit, 'edit-submit');
+    return $this->getUrl();
+  }
+
 }
