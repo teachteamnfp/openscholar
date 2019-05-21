0ef064b617712c64c13bd010efb295d202c425ba
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2019-05-21 17:17:24 +0530

    Test fix for meta tags.

--- profile/modules/custom/os_metatag/tests/src/ExistingSiteJavascript/CpSettingsOsMetatagTest.php
+++ profile/modules/custom/os_metatag/tests/src/ExistingSiteJavascript/CpSettingsOsMetatagTest.php
@@ -26,7 +26,6 @@ class CpSettingsOsMetatagTest extends OsExistingSiteJavascriptTestBase {
     parent::setUp();
     $this->groupAdmin = $this->createUser();
     $this->addGroupAdmin($this->groupAdmin, $this->group);
-    $this->cacheService = $this->container->get('cache.render');
   }
 
   /**
@@ -83,7 +82,6 @@ class CpSettingsOsMetatagTest extends OsExistingSiteJavascriptTestBase {
     $web_assert->statusCodeEquals(200);
 
     $this->drupalLogout();
-    $this->cacheService->invalidateAll();
     $this->visit($this->group->get('path')->first()->getValue()['alias']);
     $expectedHtmlValue = '<link rel="publisher" href="http://example-publisher.com/&amp;quot;&amp;#039;quote-test&amp;lt;&amp;gt;">';
     $this->assertContains($expectedHtmlValue, $this->getCurrentPageContent(), 'HTML head not contains publisher link.');
