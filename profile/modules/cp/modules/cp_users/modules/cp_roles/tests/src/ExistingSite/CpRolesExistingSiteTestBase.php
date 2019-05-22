917c36776bf980885fa432bfa1166e9c361f33e2
Subhojit Paul <subhojitpaul21@gmail.com>
2019-05-22 18:35:04 +0530

    Issue #11627: Fix failing testVsiteSpecificRole

--- profile/modules/cp/modules/cp_users/modules/cp_roles/tests/src/ExistingSite/CpRolesExistingSiteTestBase.php
+++ profile/modules/cp/modules/cp_users/modules/cp_roles/tests/src/ExistingSite/CpRolesExistingSiteTestBase.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\cp_roles\ExistingSite;
 
 use Drupal\Tests\cp_roles\Traits\CpRolesTestTrait;
 use Drupal\Tests\openscholar\ExistingSite\OsExistingSiteTestBase;
+use Drupal\vsite\Config\VsiteStorageDefinition;
 
 /**
  * Test base for CpRoles tests.
@@ -11,5 +12,15 @@ use Drupal\Tests\openscholar\ExistingSite\OsExistingSiteTestBase;
 abstract class CpRolesExistingSiteTestBase extends OsExistingSiteTestBase {
 
   use CpRolesTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    // Resets the weight. Otherwise, this breaks subsequent tests.
+    drupal_static_reset(VsiteStorageDefinition::ACTIVATED_VSITE_WEIGHT_KEY);
+
+    parent::tearDown();
+  }
 
 }
--- profile/modules/vsite/src/Config/VsiteStorageDefinition.php
+++ profile/modules/vsite/src/Config/VsiteStorageDefinition.php
@@ -11,6 +11,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class VsiteStorageDefinition implements EventSubscriberInterface {
 
+  public const ACTIVATED_VSITE_WEIGHT_KEY = 'activated_vsite_weight_key';
+
   /**
    * The storage element to add a vsite storage to.
    *
@@ -41,7 +43,7 @@ class VsiteStorageDefinition implements EventSubscriberInterface {
    * Listener for VsiteActivatedEvent.
    */
   public function onVsiteActivated(VsiteActivatedEvent $event) {
-    static $vsite_weight = 0;
+    $vsite_weight = &drupal_static(self::ACTIVATED_VSITE_WEIGHT_KEY, 0);
     $storage = $this->hierarchicalStorage->createCollection('vsite:' . $event->getGroup()->id());
     $this->hierarchicalStorage->addStorage($storage, $vsite_weight--);
   }
