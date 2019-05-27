933f3bbad57ece6b208cfead4a4fa29d3affd2fa
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-05-27 11:38:45 +0200

    Issue #11530 - Create a new non-admin multiple deletion path for bibcite reference delete form

--- profile/modules/cp/src/Plugin/Action/Derivative/CpEntityDeleteActionDeriver.php
+++ profile/modules/cp/src/Plugin/Action/Derivative/CpEntityDeleteActionDeriver.php
@@ -17,6 +17,10 @@ class CpEntityDeleteActionDeriver extends EntityDeleteActionDeriver {
   public function getDerivativeDefinitions($base_plugin_definition) {
     parent::getDerivativeDefinitions($base_plugin_definition);
 
+    if (!empty($this->derivatives['bibcite_reference'])) {
+      $this->derivatives['bibcite_reference']['confirm_form_route_name'] = 'cp.bibcite_reference.delete_multiple_form';
+    }
+
     if (!empty($this->derivatives['node'])) {
       $this->derivatives['node']['confirm_form_route_name'] = 'cp.node.multiple_delete_confirm';
     }
