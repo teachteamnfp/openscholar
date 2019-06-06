533dbbcb52903a1a9dfed8c29f822a37a635d414
Matt Petrovic <mpetrovic@guneva.net>
2019-06-06 16:23:53 -0400

    #11027: JS and css for the layout form

---
+++ profile/modules/apps/os_widgets/js/layout.form.js
@@ -0,0 +1,120 @@
+(function ($) {
+
+  Drupal.behaviors.osWidgetsLayoutForm = {
+    attach: function (context, settings) {
+      let all_contexts = drupalSettings.layoutContexts,
+        top_level = (new URLSearchParams(window.location.search)).get('context'),
+        active_limited = [],
+        context_found = false;
+
+      for (let k in all_contexts) {
+        if (k == top_level) {
+          context_found = true;
+        }
+        if (context_found) {
+          active_limited.push(k);
+        }
+      }
+
+      // swap the context that is being editted.
+      $('#block-place-context-selector', context).change(function (e) {
+        let new_context = e.target.value;
+        let vars = {}, hash;
+
+        let q = window.location.search;
+        if (q != undefined){
+          q = q.slice(1).split('&');
+          for (let i = 0; i < q.length; i++){
+            hash = q[i].split('=');
+            vars[hash[0]] = hash[1];
+          }
+        }
+
+        vars.context = new_context;
+        let query_string = $.param(vars);
+        window.location.search = '?'+query_string;
+      });
+
+      // Define regions to be sortable targets
+      $('.block-place-region', context).sortable({
+        items: '> div, > nav',
+        connectWith: '.block-place-region, #block-list',
+        placeholder: 'ui-state-drop',
+        appendTo: document.body,
+        helper: 'clone',
+        out: function (e, ui) {
+          console.log(ui);
+        }
+      });
+
+      // Define the unused widget list to be a sortable target
+      $('#block-list', context).sortable({
+        items: '> div, > nav',
+        connectWith: '.block-place-region, #block-list',
+        placeholder: 'ui-state-drop',
+        appendTo: document.body,
+        helper: 'clone',
+        out: function (e, ui) {
+          console.log(ui);
+        }
+      });
+
+      // Open the new widget panel
+      $('#create-new-widget-btn', context).click(function (e) {
+          $('#factory-wrapper').show();
+      });
+
+      $('#factory-wrapper .close', context).click(function (e) {
+        $('#factory-wrapper').hide();
+      });
+
+      // Submit the layout to the server.
+      $('#block-place-actions-wrapper .btn-success', context).click(function (e) {
+        let items = [];
+        $('.block-place-region').each(function (region) {
+          let region_name = this.attributes['data-region'].value;
+          $(this).find('> div, > nav').each(function (weight) {
+            items.push({
+              id: this.attributes['data-block-id'].value,
+              region: region_name,
+              weight: weight
+            });
+          });
+        });
+
+        let payload = {
+          contexts: active_limited,
+          blocks: items
+        };
+        let url = drupalSettings.paths.layout.saveLayout;
+        $.post(url, payload).done(function (data, status, xhr) {
+          //window.location.href = data.redirect;
+        }).fail(function (xhr, status, error) {
+
+        });
+      });
+
+      $('#block-place-actions-wrapper .btn-default', context).click(function (e) {
+        window.location.reload();
+      });
+    }
+  }
+
+  Drupal.behaviors.layoutBuilderDisableInteractiveElements = {
+    attach: function attach(context, settings) {
+      let $blocks = $('#layout-builder [data-layout-block-uuid]', context);
+      $blocks.find('input, textarea, select').prop('disabled', true);
+      $blocks.find('a').not(function (index, element) {
+        return $(element).closest('[data-contextual-id]').length > 0;
+      }).on('click mouseup touchstart', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+      });
+
+      $blocks.find('button, [href], input, select, textarea, iframe, [tabindex]:not([tabindex="-1"]):not(.tabbable)').not(function (index, element) {
+        return $(element).closest('[data-contextual-id]').length > 0;
+      }).attr('tabindex', -1);
+    }
+  };
+
+})(jQuery);
