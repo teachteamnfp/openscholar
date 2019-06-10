516c590301403970030d425c0733054840476a54
Subhojit Paul <subhojitpaul21@gmail.com>
2019-06-10 11:20:00 +0530

    Issue #11693: Group type can change

--- profile/modules/cp/modules/cp_users/modules/cp_roles/tests/src/ExistingSiteJavascript/CpRolesTest.php
+++ profile/modules/cp/modules/cp_users/modules/cp_roles/tests/src/ExistingSiteJavascript/CpRolesTest.php
@@ -39,7 +39,7 @@ class CpRolesTest extends CpRolesExistingSiteJavascriptTestBase {
   public function testCreate(): void {
     $this->drupalLogin($this->groupAdmin);
 
-    $this->visit("/{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/add");
+    $this->visit("/{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/add/{$this->group->getGroupType()->id()}");
     $this->getSession()->getPage()->fillField('Name', 'Stooges');
     $this->assertSession()->waitForElementVisible('css', '.machine-name-value');
     $this->assertSession()->pageTextContains("personal-{$this->group->id()}_stooges");
@@ -66,7 +66,7 @@ class CpRolesTest extends CpRolesExistingSiteJavascriptTestBase {
     $this->drupalLogin($this->groupAdmin);
 
     $this->visit("/{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles");
-    $group_role_edit_link = $this->getSession()->getPage()->find('css', "[href='{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/{$group_role->id()}/edit?destination={$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles']");
+    $group_role_edit_link = $this->getSession()->getPage()->find('css', "[href='{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/{$group_role->id()}/edit/{$this->group->getGroupType()->id()}?destination={$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles']");
     $group_role_edit_link->click();
 
     $this->getSession()->getPage()->fillField('Name', 'The Stooges Funhouse');
@@ -92,7 +92,7 @@ class CpRolesTest extends CpRolesExistingSiteJavascriptTestBase {
 
     $this->visit("/{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles");
 
-    $group_role_delete_link = $this->getSession()->getPage()->find('css', "[href='{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/{$group_role->id()}/delete?destination={$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles']");
+    $group_role_delete_link = $this->getSession()->getPage()->find('css', "[href='{$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles/{$group_role->id()}/delete/{$this->group->getGroupType()->id()}?destination={$this->group->get('path')->getValue()[0]['alias']}/cp/users/roles']");
     $group_role_delete_link->click();
 
     $this->getSession()->getPage()->pressButton('Delete');
