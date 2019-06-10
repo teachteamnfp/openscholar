22dae9a83faef2c7b37a8419e8d63f94de667a2f
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-06-10 15:15:40 +0530

    #11742: sass and gulpfile added

---
+++ profile/libraries/os-toolbar/gulpfile.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * Base theme gulp tasks.
+ */
+
+(function () {
+  'use strict';
+
+  var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    path = require('path');
+
+  gulp.task('sass', function () {
+    return gulp
+      .src('*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        outputStyle: 'uncompressed'
+      }).on('error', sass.logError))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('.'));
+  });
+
+})();
