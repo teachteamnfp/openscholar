18acc5e12a699582d598642e0cde8b1ec0445a07
Subhojit Paul <subhojitpaul21@gmail.com>
2019-06-13 17:26:53 +0530

    Issue #11734: CustomTheme entity test

---
+++ profile/modules/cp/modules/cp_appearance/tests/src/ExistingSite/CustomThemeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Tests\cp_appearance\ExistingSite;
+
+use Drupal\cp_appearance\Entity\CustomTheme;
+
+/**
+ * Tests custom theme entity.
+ *
+ * @group kernel
+ * @group cp-appearance
+ * @coversDefaultClass \Drupal\cp_appearance\Entity\CustomTheme
+ */
+class CustomThemeTest extends TestBase {
+
+  /**
+   * Tests custom theme save.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testSave(): void {
+    $image = $this->createFile('image');
+    $favicon = $this->createFile('image');
+
+    $custom_theme = CustomTheme::create([
+      'id' => 'test',
+      'label' => 'Test',
+    ]);
+    $custom_theme->setBaseTheme('clean');
+    $custom_theme->setFavicon($favicon->id());
+    $custom_theme->setImages([$image->id()]);
+    $custom_theme->setStyles('body { color: black; }');
+    $custom_theme->setScripts('alter("Hello World")');
+    $custom_theme->save();
+
+    $this->assertEquals('test', $custom_theme->id());
+    $this->assertEquals('Test', $custom_theme->label());
+    $this->assertEquals('clean', $custom_theme->getBaseTheme());
+    $this->assertEquals($favicon->id(), $custom_theme->getFavicon());
+    $this->assertEquals([$image->id()], $custom_theme->getImages());
+    $this->assertEquals('body { color: black; }', $custom_theme->getStyles());
+    $this->assertEquals('alter("Hello World")', $custom_theme->getScripts());
+
+    $this->markConfigForCleanUp($custom_theme);
+  }
+
+}
