156bd4ba5c3b5e3f3dd2a0ed0d5840475da10c6b
Richard Brandon <Richard.Brandon@gmail.com>
2019-06-20 15:12:25 -0400

    Add mising namespace (oops)

--- .travis/config/bootstrap.php
+++ .travis/config/bootstrap.php
@@ -21,7 +21,7 @@ list($finder, $class_loader) = AddPsr4::add();
 $root = $finder->getDrupalRoot();
 
 // Register more namespaces, as needed.
-$class_loader->addPsr4('Drupal\Tests\book\Functional\BookTestTrait\\', "$root/core/modules/book/tests/src/Functional");
+$class_loader->addPsr4('Drupal\Tests\book\Functional\\', "$root/core/modules/book/tests/src/Functional");
 $class_loader->addPsr4('Drupal\Tests\os_classes\ExistingSite\\', "$root/profiles/contrib/openscholar/modules/apps/os_classes/tests/src/ExistingSite");
 $class_loader->addPsr4('Drupal\Tests\os_classes\ExistingSiteJavascript\\', "$root/profiles/contrib/openscholar/modules/apps/os_classes/tests/src/ExistingSiteJavascript");
 $class_loader->addPsr4('Drupal\Tests\os_classes\Traits\\', "$root/profiles/contrib/openscholar/modules/apps/os_classes/tests/src/Traits");
