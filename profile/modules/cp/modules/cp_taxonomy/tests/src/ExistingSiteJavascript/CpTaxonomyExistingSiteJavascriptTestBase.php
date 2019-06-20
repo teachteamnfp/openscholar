f02087a6e2946a2ec146c5e7de3741973f3911f5
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-06-20 20:12:27 +0200

    Issue #11801 - Create test for caching (WIP)

--- profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/CpTaxonomyExistingSiteJavascriptTestBase.php
+++ profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/CpTaxonomyExistingSiteJavascriptTestBase.php
@@ -2,14 +2,39 @@
 
 namespace Drupal\Tests\cp_taxonomy\ExistingSiteJavascript;
 
+use Drupal\group\Entity\GroupInterface;
 use Drupal\group\Entity\GroupRole;
 use Drupal\group\Entity\GroupType;
+use Drupal\media\MediaInterface;
+use Drupal\node\NodeInterface;
+use Drupal\taxonomy\Entity\Vocabulary;
 use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTestBase;
 
 /**
  * Test base for cp_taxonomy js tests.
  */
 abstract class CpTaxonomyExistingSiteJavascriptTestBase extends OsExistingSiteJavascriptTestBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  /**
+   * Vsite Context Manager.
+   *
+   * @var \Drupal\vsite\Plugin\VsiteContextManagerInterface
+   */
+  protected $vsiteContextManager;
 
   protected const PERMISSIONS = [
     'create group_node:taxonomy_test_1 entity',
@@ -60,6 +85,125 @@ abstract class CpTaxonomyExistingSiteJavascriptTestBase extends OsExistingSiteJa
     $group_admin_role = GroupRole::load('personal-administrator');
     $group_admin_role->grantPermissions(self::PERMISSIONS);
     $group_admin_role->save();
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->config = $this->container->get('config.factory');
+    $this->vsiteContextManager = $this->container->get('vsite.context_manager');
+  }
+
+  /**
+   * Creates a taxonomy_test_1.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_1.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created node entity.
+   */
+  protected function createTaxonomyTest1(array $values = []) : NodeInterface {
+    $event = $this->createNode($values + [
+      'type' => 'taxonomy_test_1',
+      'title' => $this->randomString(),
+    ]);
+
+    return $event;
+  }
+
+  /**
+   * Creates a taxonomy_test_2.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_2.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created node entity.
+   */
+  protected function createTaxonomyTest2(array $values = []) : NodeInterface {
+    $event = $this->createNode($values + [
+      'type' => 'taxonomy_test_2',
+      'title' => $this->randomString(),
+    ]);
+
+    return $event;
+  }
+
+  /**
+   * Creates a taxonomy_test_file Media.
+   *
+   * @param array $values
+   *   The values used to create the taxonomy_test_file.
+   *
+   * @return \Drupal\media\MediaInterface
+   *   The created media entity.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function createTaxonomyTestFile(array $values = []) : MediaInterface {
+    $media = $this->entityTypeManager->getStorage('media')->create($values + [
+      'type' => 'taxonomy_test_file',
+      'name' => $this->randomMachineName(),
+    ]);
+    $media->enforceIsNew();
+    $media->save();
+
+    $this->markEntityForCleanup($media);
+
+    return $media;
+  }
+
+  /**
+   * Create a vocabulary to a group.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   Group entity.
+   * @param string $vid
+   *   Vocabulary id.
+   * @param array $allowed_types
+   *   Allowed types for entity bundles.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function createGroupVocabulary(GroupInterface $group, string $vid, array $allowed_types = []) {
+    $this->vsiteContextManager->activateVsite($group);
+    $vocab = Vocabulary::create([
+      'name' => $vid,
+      'vid' => $vid,
+    ]);
+    $vocab->enforceIsNew();
+    $vocab->save();
+    if (!empty($allowed_types)) {
+      $config_vocab = $this->config->getEditable('taxonomy.vocabulary.' . $vid);
+      $config_vocab
+        ->set('allowed_vocabulary_reference_types', $allowed_types)
+        ->save(TRUE);
+    }
+
+    $this->markEntityForCleanup($vocab);
+  }
+
+  /**
+   * Create a vocabulary to a group on cp taxonomy pages.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   Group entity.
+   * @param string $vid
+   *   Vocabulary id.
+   * @param string $name
+   *   Taxonomy term name.
+   *
+   * @return \Drupal\taxonomy\Entity\Term
+   *   Created taxonomy term.
+   */
+  protected function createGroupTerm(GroupInterface $group, string $vid, string $name) {
+    $this->vsiteContextManager->activateVsite($group);
+    $vocab = Vocabulary::load($vid);
+    $term = $this->createTerm($vocab, [
+      'name' => $name,
+    ]);
+    $group->addContent($term, 'group_entity:taxonomy_term');
+    return $term;
   }
 
   /**
--- profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/TaxonomyTermsFieldTest.php
+++ profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSiteJavascript/TaxonomyTermsFieldTest.php
@@ -2,9 +2,6 @@
 
 namespace Drupal\Tests\cp_taxonomy\ExistingSiteJavascript;
 
-use Drupal\group\Entity\GroupInterface;
-use Drupal\media\MediaInterface;
-use Drupal\node\NodeInterface;
 use Drupal\taxonomy\Entity\Vocabulary;
 
 /**
@@ -14,12 +11,6 @@ use Drupal\taxonomy\Entity\Vocabulary;
  * @group cp
  */
 class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
-  /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
 
   /**
    * Test group 1.
@@ -35,19 +26,6 @@ class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
    */
   protected $group2;
 
-  /**
-   * Config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $config;
-
-  /**
-   * Vsite Context Manager.
-   *
-   * @var \Drupal\vsite\Plugin\VsiteContextManagerInterface
-   */
-  protected $vsiteContextManager;
 
   /**
    * Group administrator.
@@ -61,9 +39,6 @@ class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
    */
   public function setUp() {
     parent::setUp();
-    $this->entityTypeManager = $this->container->get('entity_type.manager');
-    $this->config = $this->container->get('config.factory');
-    $this->vsiteContextManager = $this->container->get('vsite.context_manager');
 
     $this->group1 = $this->createGroup([
       'path' => [
@@ -149,117 +124,6 @@ class TaxonomyTermsFieldTest extends CpTaxonomyExistingSiteJavascriptTestBase {
     $page = $this->getCurrentPage();
     $is_exists = $page->hasContent('Tag with Terms');
     $this->assertFalse($is_exists, 'Tag with Terms field is visible.');
-  }
-
-  /**
-   * Creates a taxonomy_test_1.
-   *
-   * @param array $values
-   *   The values used to create the taxonomy_test_1.
-   *
-   * @return \Drupal\node\NodeInterface
-   *   The created node entity.
-   */
-  protected function createTaxonomyTest1(array $values = []) : NodeInterface {
-    $event = $this->createNode($values + [
-      'type' => 'taxonomy_test_1',
-      'title' => $this->randomString(),
-    ]);
-
-    return $event;
-  }
-
-  /**
-   * Creates a taxonomy_test_2.
-   *
-   * @param array $values
-   *   The values used to create the taxonomy_test_2.
-   *
-   * @return \Drupal\node\NodeInterface
-   *   The created node entity.
-   */
-  protected function createTaxonomyTest2(array $values = []) : NodeInterface {
-    $event = $this->createNode($values + [
-      'type' => 'taxonomy_test_2',
-      'title' => $this->randomString(),
-    ]);
-
-    return $event;
-  }
-
-  /**
-   * Creates a taxonomy_test_file Media.
-   *
-   * @param array $values
-   *   The values used to create the taxonomy_test_file.
-   *
-   * @return \Drupal\media\MediaInterface
-   *   The created media entity.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function createTaxonomyTestFile(array $values = []) : MediaInterface {
-    $media = $this->entityTypeManager->getStorage('media')->create($values + [
-      'type' => 'taxonomy_test_file',
-      'name' => $this->randomMachineName(),
-    ]);
-    $media->enforceIsNew();
-    $media->save();
-
-    $this->markEntityForCleanup($media);
-
-    return $media;
-  }
-
-  /**
-   * Create a vocabulary to a group.
-   *
-   * @param \Drupal\group\Entity\GroupInterface $group
-   *   Group entity.
-   * @param string $vid
-   *   Vocabulary id.
-   * @param array $allowed_types
-   *   Allowed types for entity bundles.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function createGroupVocabulary(GroupInterface $group, string $vid, array $allowed_types = []) {
-    $this->vsiteContextManager->activateVsite($group);
-    $vocab = Vocabulary::create([
-      'name' => $vid,
-      'vid' => $vid,
-    ]);
-    $vocab->enforceIsNew();
-    $vocab->save();
-    if (!empty($allowed_types)) {
-      $config_vocab = $this->config->getEditable('taxonomy.vocabulary.' . $vid);
-      $config_vocab
-        ->set('allowed_vocabulary_reference_types', $allowed_types)
-        ->save(TRUE);
-    }
-
-    $this->markEntityForCleanup($vocab);
-  }
-
-  /**
-   * Create a vocabulary to a group on cp taxonomy pages.
-   *
-   * @param \Drupal\group\Entity\GroupInterface $group
-   *   Group entity.
-   * @param string $vid
-   *   Vocabulary id.
-   * @param string $name
-   *   Taxonomy term name.
-   */
-  protected function createGroupTerm(GroupInterface $group, string $vid, string $name) {
-    $this->vsiteContextManager->activateVsite($group);
-    $vocab = Vocabulary::load($vid);
-    $term = $this->createTerm($vocab, [
-      'name' => $name,
-    ]);
-    $group->addContent($term, 'group_entity:taxonomy_term');
   }
 
   /**
