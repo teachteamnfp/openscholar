3d1b1dd00b9e299c5c35ee24f63270bc505f5519
Subhojit Paul <subhojitpaul21@gmail.com>
2019-07-01 12:27:09 +0530

    Issue #11736: Debug - Travis failure

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -373,6 +373,7 @@ class RoboFile extends \Robo\Tasks
         $groups = implode(',', $groups);
         $tasks[] = $this->taskExecStack()
             ->exec('docker-compose exec -T php ls -al')
+            ->exec('docker-compose exec -T php ls -al custom_themes')
             ->exec('docker-compose exec -T php ./vendor/bin/phpunit ' .
                 '-c web/core '.
                 '--debug '.
