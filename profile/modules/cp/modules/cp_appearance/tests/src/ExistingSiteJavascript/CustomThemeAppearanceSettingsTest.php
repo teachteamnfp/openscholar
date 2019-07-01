5a884fcac6bd564725d3d30fdb27427d2d518c9e
Subhojit Paul <subhojitpaul21@gmail.com>
2019-07-01 18:07:32 +0530

    Issue #11736: Debug - Travis failure

--- profile/modules/cp/modules/cp_appearance/tests/src/ExistingSiteJavascript/CustomThemeAppearanceSettingsTest.php
+++ profile/modules/cp/modules/cp_appearance/tests/src/ExistingSiteJavascript/CustomThemeAppearanceSettingsTest.php
@@ -10,6 +10,7 @@ use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTest
  *
  * @group functional-javascript
  * @group cp-appearance
+ * @group unique
  * @coversDefaultClass \Drupal\cp_appearance\Controller\CpAppearanceMainController
  */
 class CustomThemeAppearanceSettingsTest extends OsExistingSiteJavascriptTestBase {
