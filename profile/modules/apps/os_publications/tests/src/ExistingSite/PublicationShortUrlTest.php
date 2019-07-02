9c6483dc02e630d6d42e58b808a697ba3822a74c
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2019-07-02 17:41:43 +0530

    Debug test failure.

--- profile/modules/apps/os_publications/tests/src/ExistingSite/PublicationShortUrlTest.php
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/PublicationShortUrlTest.php
@@ -2,18 +2,18 @@
 
 namespace Drupal\Tests\os_publications\ExistingSite;
 
-use Drupal\Tests\openscholar\ExistingSite\OsExistingSiteTestBase;
+use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTestBase;
 
 /**
  * Class PublicationShortUrlTest.
  *
  * @group functional
- * @group test-test
+ * @group publications
  *
  * @package Drupal\Tests\os_publications\ExistingSite
  * @covers \os_publications_preprocess_bibcite_citation
  */
-class PublicationShortUrlTest extends OsExistingSiteTestBase {
+class PublicationShortUrlTest extends OsExistingSiteJavascriptTestBase {
   /**
    * Group administrator.
    *
@@ -27,13 +27,17 @@ class PublicationShortUrlTest extends OsExistingSiteTestBase {
   public function setUp() {
     parent::setUp();
 
+    $this->group = $this->createGroup([
+      'path' => [
+        'alias' => '/test-menu',
+      ],
+    ]);
     $this->groupAdmin = $this->createUser();
     $this->addGroupAdmin($this->groupAdmin, $this->group);
     $this->drupalLogin($this->groupAdmin);
     $this->reference = $this->createReference([
       'title' => 'The Velvet Underground',
     ]);
-    $this->cache = $this->container->get('cache.render');
   }
 
   /**
@@ -44,26 +48,24 @@ class PublicationShortUrlTest extends OsExistingSiteTestBase {
    */
   public function testPublicationShortUrl(): void {
 
-    // Test citations do not occur if citations is set to half.
-    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/cp/settings/publications");
+    // Test citations do not occur if short citations is off.
+    $this->visit("/test-menu/cp/settings/publications");
     $edit = [
       'os_publications_shorten_citations' => FALSE,
     ];
     $this->submitForm($edit, 'edit-submit');
     $ref_url = $this->reference->toUrl()->toString();
-    $this->cache->invalidateAll();
-    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/$ref_url");
+    $this->visit("/test-menu/$ref_url");
     $web_assert = $this->assertSession();
     $web_assert->elementNotExists('css', '.short-link');
 
     // Test citations occur when short url setting is on.
-    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/cp/settings/publications");
+    $this->drupalGet("/test-menu/cp/settings/publications");
     $edit = [
       'os_publications_shorten_citations' => TRUE,
     ];
     $this->submitForm($edit, 'edit-submit');
-    $this->cache->invalidateAll();
-    $this->drupalGet("{$this->group->get('path')->first()->getValue()['alias']}/$ref_url");
+    $this->visit("/test-menu/$ref_url");
     $web_assert->elementExists('css', '.short-link');
   }
 
