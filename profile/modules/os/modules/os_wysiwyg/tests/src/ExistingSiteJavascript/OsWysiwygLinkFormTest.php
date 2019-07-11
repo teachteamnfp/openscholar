303215c7debba47aa6f4cbab3ff5f199b55b0980
Matt Petrovic <mpetrovic@guneva.net>
2019-07-11 10:26:34 -0400

    #11297: First test passes

--- profile/modules/os/modules/os_wysiwyg/tests/src/ExistingSiteJavascript/OsWysiwygLinkFormTest.php
+++ profile/modules/os/modules/os_wysiwyg/tests/src/ExistingSiteJavascript/OsWysiwygLinkFormTest.php
@@ -20,32 +20,31 @@ class OsWysiwygLinkFormTest extends OsExistingSiteJavascriptTestBase {
   }
 
   public function testLinkForm() {
-    error_log('login');
     $this->drupalLogin($this->user);
-error_log('visit');
-    $this->drupalGet($this->groupAlias . '/node/add/page');
+    $this->drupalGet($this->getAbsoluteUrl($this->groupAlias . '/node/add/page'));
 
-    error_log('open form');
-    $page = $this->getSession()->getPage();
-    $button = $page->find('css', '.cke_button__oslink');
-    $button->click();
-    error_log('wait for element');
-    $this->assertSession()->waitForElement('css', '#owl-field-text');
-    error_log('element open');
+    $this->assertSession()->waitForElement('css', '.cke_button__oslink');
 
-    $dialog = $page->find('css', '.wysiwyg-link-tool-wrapper');
+    $dialog = $this->getLinkForm();
     $dialog->fillField('Link Text', 'Internal URL');
     $dialog->fillField('Title Text', 'Internal Title');
     $dialog->fillfield('Website URL', 'about');
     $dialog->checkField('Open this link in a new tab');
     $dialog->findButton('Insert')->click();
 
-    $this->assertWysiwygContains('edit-body-0-value', '<a data-cke-saved-href="about" href="about" data-url="about" target="_blank" title="Internal Title">Internal URL</a>');
-
+    $this->assertWysiwygContains('edit-body-0-value', '<a data-url="about" href="about" target="_blank" title="Internal Title">Internal URL</a>');
   }
 
   protected function assertWysiwygContains($wysiwyg, $content) {
-    $return = $this->getSession()->evaluateScript('CKEDITOR.instances['.$wysiwyg.'].getData()');
+    $return = $this->getSession()->evaluateScript('CKEDITOR.instances["'.$wysiwyg.'"].getData()');
     $this->assertContains($content, $return, "Content not found, or wysiwyg with that id does not exist.");
   }
+
+  protected function getLinkForm() {
+    $button = $this->getSession()->getPage()->find('css', '.cke_button__oslink');
+    $button->click();
+    $this->assertSession()->waitForElement('css', '#owl-field-text');
+    return $this->getSession()->getPage()->find('css', '.wysiwyg-link-tool-wrapper');
+  }
+
 }
