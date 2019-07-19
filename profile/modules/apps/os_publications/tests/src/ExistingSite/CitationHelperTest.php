f4f8852b90d3328f60429928d8d30d4113cb0019
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2019-07-19 17:32:07 +0530

    Removing unwanted code.

--- profile/modules/apps/os_publications/tests/src/ExistingSite/CitationHelperTest.php
+++ profile/modules/apps/os_publications/tests/src/ExistingSite/CitationHelperTest.php
@@ -20,34 +20,11 @@ class CitationHelperTest extends TestBase {
   protected $citationHelper;
 
   /**
-   * Reference content.
-   *
-   * @var \Drupal\bibcite_entity\Entity\ReferenceInterface
-   */
-  protected $ref1;
-
-  /**
-   * Reference content.
-   *
-   * @var \Drupal\bibcite_entity\Entity\ReferenceInterface
-   */
-  protected $ref2;
-
-  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
     $this->citationHelper = $this->container->get('os_publications.citation_helper');
-    $this->ref1 = $this->createReference([
-      'html_title' => 'This is export test',
-    ]);
-    $this->ref2 = $this->createReference([
-      'html_title' => 'This is another export test',
-      'type' => 'miscellaneous',
-    ]);
-    $this->group->addContent($this->ref1, 'group_entity:bibcite_reference');
-    $this->group->addContent($this->ref2, 'group_entity:bibcite_reference');
   }
 
   /**
