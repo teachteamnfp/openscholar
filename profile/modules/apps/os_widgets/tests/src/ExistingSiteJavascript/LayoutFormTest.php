4fde2fcd53c62d7fbc930db32dc1e417edc48e02
Matt Petrovic <mpetrovic@guneva.net>
2019-07-23 15:46:56 -0400

    #11911: Test that the widget filtering functionality works.

---
+++ profile/modules/apps/os_widgets/tests/src/ExistingSiteJavascript/LayoutFormTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\Tests\os_widgets\ExistingSiteJavascript;
+
+
+use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTestBase;
+use Drupal\Tests\os_widgets\Traits\WidgetCreationTrait;
+
+class LayoutFormTest extends OsExistingSiteJavascriptTestBase {
+
+  use WidgetCreationTrait;
+
+  protected $user;
+
+  public function setUp() {
+    parent::setUp();
+    $this->user = $this->createUser();
+    $this->group->addMember($this->user, [
+      'group_roles' => [
+        'personal-administrator'
+      ]
+    ]);
+  }
+
+  public function testFilterWidget() {
+    $blocks[] = $this->createBlockContent([
+      'info' => 'abcdef'
+    ]);
+
+    $blocks[] = $this->createBlockContent([
+      'info' => 'ghijkl'
+    ]);
+
+    foreach ($blocks as $b) {
+      $plugin_id = 'block_content:' . $b->uuid();
+      $block_id = 'block_content|' . $b->uuid();
+      $block = \Drupal::entityTypeManager()->getStorage('block')->create(['plugin' => $plugin_id, 'id' => $block_id]);
+      $block->save();
+
+      $this->group->addContent($b, 'group_entity:block_content');
+    }
+
+    $this->drupalLogin($this->user);
+    $this->visitViaVsite('blog', $this->group);
+    $this->getSession()->getDriver()->click('//a[contains(.,"Place block")]');
+
+    $this->assertSession()->pageTextContains('Filter Widgets');
+    $this->assertSession()->pageTextContains('abcdef');
+    $this->assertSession()->pageTextContains('ghijkl');
+
+    $this->getSession()->getPage()->fillField('filter-widgets', 'abcdef');
+    $this->getSession()->executeScript('document.querySelector("#block-place-widget-selector-wrapper").scrollTo(5, 5);');
+    $this->assertTrue($this->getSession()->getPage()->find('xpath', '//h3[contains(.,"abcdef")]')->isVisible());
+    $this->assertNotTrue($this->getSession()->getPage()->find('xpath', '//h3[contains(.,"ghijkl")]')->isVisible());
+  }
+}
