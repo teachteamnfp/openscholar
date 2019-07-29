901a52fc9abea6534f1dc5b892c88c6a815a734c
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-07-29 10:27:19 +0200

    Issue #11801 - Fix caching on taxonomy term reference view field render

--- profile/modules/cp/modules/cp_taxonomy/src/Plugin/views/field/ReferenceTagWithTerms.php
+++ profile/modules/cp/modules/cp_taxonomy/src/Plugin/views/field/ReferenceTagWithTerms.php
@@ -5,6 +5,7 @@ namespace Drupal\cp_taxonomy\Plugin\views\field;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Drupal\vsite\Plugin\VsiteContextManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -22,13 +23,15 @@ class ReferenceTagWithTerms extends FieldPluginBase {
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $configFactory;
+  private $vsiteContextManager;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, VsiteContextManagerInterface $vsite_context_manager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->configFactory = $config_factory;
+    $this->vsiteContextManager = $vsite_context_manager;
   }
 
   /**
@@ -39,7 +42,8 @@ class ReferenceTagWithTerms extends FieldPluginBase {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('vsite.context_manager')
     );
   }
 
@@ -57,10 +61,15 @@ class ReferenceTagWithTerms extends FieldPluginBase {
     $reference = $this->getEntity($values);
     $config = $this->configFactory->get('cp_taxonomy.settings');
     $display_term_under_content_teaser_types = $config->get('display_term_under_content_teaser_types');
+    $build = [];
     if (is_null($display_term_under_content_teaser_types) || in_array($reference->getEntityType()->id() . ':*', $display_term_under_content_teaser_types)) {
       return $reference->field_taxonomy_terms->view(['label' => 'hidden']);
     }
-    return '';
+    if (!empty($reference->field_taxonomy_terms->referencedEntities())) {
+      $group = $this->vsiteContextManager->getActiveVsite();
+      $build['#cache']['tags'][] = 'entity-with-taxonomy-terms:' . $group->id();
+    }
+    return $build;
   }
 
 }
