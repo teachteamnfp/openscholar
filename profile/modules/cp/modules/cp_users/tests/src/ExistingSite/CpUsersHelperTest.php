0f3a6b9b28a9e1d56201ae749d7287ff04f91cda
Subhojit Paul <subhojitpaul21@gmail.com>
2019-08-01 17:02:59 +0530

    Issue #11997: CpUsersHelper test

---
+++ profile/modules/cp/modules/cp_users/tests/src/ExistingSite/CpUsersHelperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\Tests\cp_users\ExistingSite;
+
+/**
+ * CpUsersHelper test.
+ *
+ * @coversDefaultClass \Drupal\cp_users\CpUsersHelper
+ */
+class CpUsersHelperTest extends CpUsersExistingSiteTestBase {
+
+  /**
+   * @covers ::isVsiteOwner
+   */
+  public function test(): void {
+    $vsite_owner = $this->createUser();
+    $this->group->setOwner($vsite_owner)->save();
+    $non_vsite_owner = $this->createUser();
+    /** @var \Drupal\cp_users\CpUsersHelperInterface $cp_users_helper */
+    $cp_users_helper = $this->container->get('cp_users.cp_users_helper');
+
+    $this->assertTrue($cp_users_helper->isVsiteOwner($this->group, $vsite_owner));
+    $this->assertFalse($cp_users_helper->isVsiteOwner($this->group, $non_vsite_owner));
+  }
+
+}
