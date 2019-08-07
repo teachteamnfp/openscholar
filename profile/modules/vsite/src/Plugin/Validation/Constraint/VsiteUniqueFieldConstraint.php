e8c14d6f0e2fd824e82ea4dd61a3de8b1234d043
Matt Petrovic <mpetrovic@guneva.net>
2019-08-07 14:35:48 -0400

    #11912: New Field Constraint to limit validation to vsite-specific values only.

---
+++ profile/modules/vsite/src/Plugin/Validation/Constraint/VsiteUniqueFieldConstraint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\vsite\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Constraint(
+ *   id = "VsiteUniqueField",
+ *   label = @Translation("Unique to a given vsite")
+ * )
+ */
+class VsiteUniqueFieldConstraint extends Constraint {
+
+  public $message = 'A @entity_type with @field_name %value already exists.';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validatedBy() {
+    return '\Drupal\vsite\Plugin\Validation\Constraint\VsiteUniqueFieldValueValidator';
+  }
+
+}
---
+++ profile/modules/vsite/src/Plugin/Validation/Constraint/VsiteUniqueFieldValueValidator.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\vsite\Plugin\Validation\Constraint;
+
+
+use Drupal\Core\Config\Entity\ConfigEntityType;
+use Drupal\group\Entity\GroupContentType;
+use Drupal\vsite\Plugin\VsiteContextManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class VsiteUniqueFieldValueValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($items, Constraint $constraint) {
+    if (!$item = $items->first()) {
+      return;
+    }
+
+    /** @var VsiteContextManagerInterface $vsiteContextManager */
+    $vsiteContextManager = \Drupal::service('vsite.context_manager');
+
+    $field_name = $items->getFieldDefinition()->getName();
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
+    $entity = $items->getEntity();
+    $entity_type_id = $entity->getEntityTypeId();
+    $id_key = $entity->getEntityType()->getKey('id');
+
+    $query = \Drupal::entityQuery($entity_type_id);
+    $group = $vsiteContextManager->getActiveVsite();
+
+    // Handle vsite things.
+    // How an entity is attached to a vsite differs based on whether its a config or content entity.
+    if ($entity->getEntityType() instanceof ConfigEntityType) {
+      if ($group) {
+        $query->condition('collection', 'vsite:'.$group->id());
+      }
+      else {
+        $query->condition('collection', '');
+      }
+    }
+    else {
+      // No join on EntityQuery, so instead we only include entities that are part of the active group.
+      /** @var GroupContentType[] $plugins */
+      $plugins = GroupContentType::loadByEntityTypeId($entity_type_id);
+      $plugin = reset($plugins);
+      if ($group) {
+        $entities = $group->getContentEntities($plugin->getContentPluginId());
+        $include = [];
+        foreach ($entities as $et) {
+          $include[] = $et->id();
+        }
+        if (count($include)) {
+          $query->condition($entity->getEntityType()->getKey('id'), $include, 'IN');
+        }
+        else {
+          // This vsite has no entities of the given type. It's value is valid by default.
+          return;
+        }
+      }
+    }
+
+    $entity_id = $entity->id();
+    // Using isset() instead of !empty() as 0 and '0' are valid ID values for
+    // entity types using string IDs.
+    if (isset($entity_id)) {
+      $query->condition($id_key, $entity_id, '<>');
+    }
+
+    $value_taken = (bool) $query
+      ->condition($field_name, $item->value)
+      ->range(0, 1)
+      ->count()
+      ->execute();
+
+    if ($value_taken) {
+      $this->context->addViolation($constraint->message, [
+        '%value' => $item->value,
+        '@entity_type' => $entity->getEntityType()->getLowercaseLabel(),
+        '@field_name' => mb_strtolower($items->getFieldDefinition()->getLabel()),
+      ]);
+    }
+  }
+
+}
