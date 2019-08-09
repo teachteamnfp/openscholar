05db273931b4e18b46698c80bec9f02d37b9974d
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-08-09 16:52:01 +0200

    Issue #11599 - Write test checking edit pages with/without destination

---
+++ profile/modules/vsite/tests/src/ExistingSite/FormDeleteDestinationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\Tests\vsite\ExistingSite;
+
+/**
+ * Test the FormDeleteDestination.
+ *
+ * @group vsite
+ * @group functional
+ */
+class FormDeleteDestinationTest extends VsiteExistingSiteTestBase {
+
+  public function setUp() {
+    parent::setUp();
+    $group_admin = $this->createUser();
+    $this->addGroupAdmin($group_admin, $this->group);
+    $this->drupalLogin($group_admin);
+  }
+
+  /**
+   * Test node edit page with destination.
+   */
+  public function testNodeEditDeleteWithDestination() {
+    $node = $this->createNode([
+      'type' => 'blog',
+    ]);
+    $this->group->addContent($node, 'group_node:blog');
+    $this->visitViaVsite('node/' . $node->id() . '/edit?destination=/hello', $this->group);
+    $page = $this->getCurrentPageContent();
+    $this->assertContains($this->groupAlias . '/node/' . $node->id() . '/delete?destination=' . $this->groupAlias . '/blog', $page);
+  }
+
+  /**
+   * Test node edit page without destination.
+   */
+  public function testNodeEditDeleteWithoutDestination() {
+    $node = $this->createNode([
+      'type' => 'blog',
+    ]);
+    $this->group->addContent($node, 'group_node:blog');
+    $this->visitViaVsite('node/' . $node->id() . '/edit', $this->group);
+    $page = $this->getCurrentPageContent();
+    $this->assertContains($this->groupAlias . '/node/' . $node->id() . '/delete?destination=' . $this->groupAlias . '/blog', $page);
+  }
+
+  /**
+   * Test reference edit page with destination.
+   */
+  public function testReferenceEditDeleteWithDestination() {
+    $publication = $this->createReference();
+    $this->group->addContent($publication, 'group_entity:bibcite_reference');
+    $this->visitViaVsite('bibcite/reference/' . $publication->id() . '/edit?destination=/hello', $this->group);
+    $page = $this->getCurrentPageContent();
+    $this->assertContains($this->groupAlias . '/bibcite/reference/' . $publication->id() . '/delete?destination=' . $this->groupAlias . '/publications', $page);
+  }
+
+  /**
+   * Test reference edit page without destination.
+   */
+  public function testReferenceEditDeleteWithoutDestination() {
+    $publication = $this->createReference();
+    $this->group->addContent($publication, 'group_entity:bibcite_reference');
+    $this->visitViaVsite('bibcite/reference/' . $publication->id() . '/edit', $this->group);
+    $page = $this->getCurrentPageContent();
+    $this->assertContains($this->groupAlias . '/bibcite/reference/' . $publication->id() . '/delete?destination=' . $this->groupAlias . '/publications', $page);
+  }
+
+}
