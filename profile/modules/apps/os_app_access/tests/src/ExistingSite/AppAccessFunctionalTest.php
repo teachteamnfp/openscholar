a52e99dc53a87c7d01cacb27d4ea4714ade81ead
Subhojit Paul <subhojitpaul21@gmail.com>
2019-08-16 12:20:29 +0530

    Issue #11638: Private app access test

--- profile/modules/apps/os_app_access/tests/src/ExistingSite/AppAccessFunctionalTest.php
+++ profile/modules/apps/os_app_access/tests/src/ExistingSite/AppAccessFunctionalTest.php
@@ -5,10 +5,39 @@ namespace Drupal\Tests\os_app_access\ExistingSite;
 /**
  * AppAccessFunctionalTest.
  *
+ * @covers \Drupal\os_app_access\Access\AppAccess
+ * @covers \Drupal\os_app_access\Plugin\views\access\AppAccess
+ * @covers \Drupal\os_app_access\Form\AppAccessForm
  * @group functional
  * @group os
  */
 class AppAccessFunctionalTest extends AppAccessTestBase {
+
+  /**
+   * Group admin.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $groupAdmin;
+
+  /**
+   * Group member.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $groupMember;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->groupAdmin = $this->createUser();
+    $this->groupMember = $this->createUser();
+    $this->addGroupAdmin($this->groupAdmin, $this->group);
+    $this->group->addMember($this->groupMember);
+  }
 
   /**
    * @covers \Drupal\os_app_access\Access\AppAccess::access
@@ -18,13 +47,10 @@ class AppAccessFunctionalTest extends AppAccessTestBase {
    * @covers ::os_app_access_node_access
    * @covers ::_os_app_access_node_type_access
    *
-   * @throws \Drupal\Core\Entity\EntityStorageException
    * @throws \Behat\Mink\Exception\ExpectationException
    */
   public function testNode(): void {
     // Setup.
-    $group_admin = $this->createUser();
-    $this->addGroupAdmin($group_admin, $this->group);
     $news = $this->createNode([
       'type' => 'news',
       'field_date' => [
@@ -41,28 +67,46 @@ class AppAccessFunctionalTest extends AppAccessTestBase {
     $this->assertSession()->statusCodeEquals(200);
 
     // Test disabled app setting.
-    $this->drupalLogin($group_admin);
+    $this->drupalLogin($this->groupAdmin);
     $this->visitViaVsite('cp/settings/app-access', $this->group);
-    $this->submitForm([
-      'enabled[blog][privacy]' => 0,
-      'enabled[class][privacy]' => 0,
-      'enabled[event][privacy]' => 0,
-      'enabled[faq][privacy]' => 0,
-      'enabled[links][privacy]' => 0,
-      'enabled[news][privacy]' => 1,
-      'enabled[page][privacy]' => 0,
-      'enabled[presentations][privacy]' => 0,
-      'enabled[profiles][privacy]' => 0,
-      'enabled[publications][privacy]' => 0,
-      'enabled[software][privacy]' => 0,
-    ], 'Save configuration');
+    $this->getSession()->getPage()->find('css', 'input[type=checkbox][name="enabled[news][disable]"]')->check();
+    $this->getSession()->getPage()->pressButton('Save configuration');
 
     $this->drupalLogout();
-
     $this->visitViaVsite("node/{$news->id()}", $this->group);
     $this->assertSession()->statusCodeEquals(403);
     $this->visitViaVsite('news', $this->group);
     $this->assertSession()->statusCodeEquals(403);
+
+    $this->drupalLogin($this->groupMember);
+    $this->visitViaVsite("node/{$news->id()}", $this->group);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->visitViaVsite('news', $this->group);
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Test enabled app setting.
+    $this->drupalLogin($this->groupAdmin);
+    $this->visitViaVsite('cp/settings/app-access', $this->group);
+    $this->getSession()->getPage()->find('css', 'input[type=checkbox][name="disabled[news][enable]"]')->check();
+    $this->getSession()->getPage()->pressButton('Save configuration');
+
+    $this->drupalLogout();
+    $this->visitViaVsite("node/{$news->id()}", $this->group);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->visitViaVsite('news', $this->group);
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Test private app setting.
+    $this->drupalLogin($this->groupAdmin);
+    $this->visitViaVsite('cp/settings/app-access', $this->group);
+    $this->getSession()->getPage()->selectFieldOption('enabled[news][privacy]', 1);
+    $this->getSession()->getPage()->pressButton('Save configuration');
+
+    $this->drupalLogin($this->groupMember);
+    $this->visitViaVsite("node/{$news->id()}", $this->group);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->visitViaVsite('news', $this->group);
+    $this->assertSession()->statusCodeEquals(200);
   }
 
   /**
@@ -77,8 +121,6 @@ class AppAccessFunctionalTest extends AppAccessTestBase {
    */
   public function testPublications(): void {
     // Setup.
-    $group_admin = $this->createUser();
-    $this->addGroupAdmin($group_admin, $this->group);
     $reference = $this->createReference();
     $this->group->addContent($reference, 'group_entity:bibcite_reference');
 
@@ -90,7 +132,7 @@ class AppAccessFunctionalTest extends AppAccessTestBase {
     $this->assertSession()->statusCodeEquals(200);
 
     // Test disabled app setting.
-    $this->drupalLogin($group_admin);
+    $this->drupalLogin($this->groupAdmin);
     $this->visitViaVsite('cp/settings/app-access', $this->group);
     $this->submitForm([
       'enabled[blog][privacy]' => 0,
@@ -112,6 +154,8 @@ class AppAccessFunctionalTest extends AppAccessTestBase {
     $this->assertSession()->statusCodeEquals(403);
     $this->visitViaVsite('publications', $this->group);
     $this->assertSession()->statusCodeEquals(403);
+
+    // TODO: Write private app setting test.
   }
 
 }
