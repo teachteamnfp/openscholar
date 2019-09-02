8dd33d2894dfbd2d7b2f65cb4885dee2fce15ef4
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2019-09-02 15:54:38 +0530

    #11600 - Reset taxonomy vocab form and functionality.

--- profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSite/CpTaxonomyVocabResetFormTest.php
+++ profile/modules/cp/modules/cp_taxonomy/tests/src/ExistingSite/CpTaxonomyVocabResetFormTest.php
@@ -61,7 +61,7 @@ class CpTaxonomyVocabResetFormTest extends TestBase {
    *
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
-  public function testCpVocabResetForm(): void {
+  public function testCpVocabularyResetForm(): void {
     $vid = 'test_vocab';
     $this->createGroupVocabulary($this->group, $vid, ['node:taxonomy_test_1']);
     $term1 = $this->createGroupTerm($this->group, $vid, 'Aterm');
@@ -80,7 +80,7 @@ class CpTaxonomyVocabResetFormTest extends TestBase {
     $this->assertEquals($term3->id(), $tree[1]->tid, 'Term 3 was moved above Term 1.');
     $this->assertEquals($term1->id(), $tree[2]->tid, 'Term 1 was moved below Term 3.');
 
-    // Reset alphabetical.
+    // Reset alphabetical via Reset Form.
     $this->visitViaVsite('cp/taxonomy/' . $vid, $this->group);
     $this->submitForm([], 'edit-reset-alphabetical');
     $this->submitForm([], 'edit-submit');
@@ -91,10 +91,10 @@ class CpTaxonomyVocabResetFormTest extends TestBase {
     $this->assertEquals($term2->id(), $tree[1]->id(), 'Term 2 was moved below Term 1.');
     $this->assertEquals($term3->id(), $tree[2]->id(), 'Term 3 was moved below Term 2.');
 
-    // Test Cancel button.
+    // Test Cancel Link.
     $url = $this->getSession()->getCurrentUrl();
     $this->submitForm([], 'edit-reset-alphabetical');
-    $this->getCurrentPage()->clickLink('#edit-cancel');
+    $this->getCurrentPage()->clickLink('Cancel');
     $current_url = $this->getSession()->getCurrentUrl();
     $this->assertEquals($url, $current_url);
 
